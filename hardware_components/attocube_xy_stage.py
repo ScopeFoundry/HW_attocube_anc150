9082d4e498b239fe04e1fdd2e25d9ea364dcd76e
Joel Grebel <joel.g.07@gmail.com>
2014-07-28 15:40:01 -0700

    add functions

--- hardware_components/attocube_xy_stage.py
+++ hardware_components/attocube_xy_stage.py
@@ -36,6 +36,30 @@ class AttoCubeXYStage(HardwareComponent):
         self.x_step_voltage = self.add_logged_quantity("x_step_volatage",
                                                        dtype=float,
                                                        ro=False)
+        self.y_step_voltage = self.add_logged_quantity("y_step_voltage",
+                                                       dtype=float,
+                                                       ro=False)
+        self.x_openloop_voltage = self.add_logged_quantity("x_openloop_voltage",
+                                                           dtype=float,
+                                                           ro=False)
+        self.y_openloop_voltage = self.add_logged_quantity("y_openloop_voltage",
+                                                           dtype=float,
+                                                           ro=False)
+        
+        self.x_frequency = self.add_logged_quantity("x_frequency",
+                                                    dtype=float,
+                                                    ro=False)
+        
+        self.y_frequency = self.add_logged_quantity("y_frequency",
+                                                    dtype=float,
+                                                    ro=False)
+        
+        self.x_actor_info = self.add_logged_quantity(name='x_actor_info', dtype=str, fmt="%s",ro=True)
+        
+        self.y_actor_info = self.add_logged_quantity(name='y_actor_info', dtype=str, fmt="%s",ro=True)
+        
+        self.electrically_connected = self.add_logged_quantity("electrically_connected", dtype=bool, 
+                                                     ro=False, initial=False)
         
         # need enable boolean lq's
         
@@ -50,11 +74,34 @@ class AttoCubeXYStage(HardwareComponent):
         
         # connect logged quantities
         self.x_position.hardware_read_func = lambda:  self.ecc100.read_position_axis(X_AXIS)
-        #self.x_position.hardware_set_func  = lambda x: self.ecc100.set_position_axis(X_AXIS, x)
+        self.x_position.hardware_set_func  = lambda x: self.ecc100.set_position_axis(X_AXIS, x)
         
         self.y_position.hardware_read_func = lambda:  self.ecc100.read_position_axis(Y_AXIS)
-        #self.y_position.hardware_set_func  = lambda y: self.ecc100.set_position_axis(Y_AXIS, y)
+        self.y_position.hardware_set_func  = lambda y: self.ecc100.set_position_axis(Y_AXIS, y)
+
+        self.x_step_voltage.hardware_read_func = lambda:  self.ecc100.read_step_voltage(X_AXIS)
         
+        self.y_step_voltage.hardware_read_func = lambda:  self.ecc100.read_step_voltage(Y_AXIS)
+        
+        self.x_openloop_voltage.hardware_read_func = lambda:  self.ecc100.read_openloop_voltage(X_AXIS)
+        self.x_openloop_voltage.hardware_set_func  = lambda x: self.ecc100.write_openloop_voltage(X_AXIS, x)
+        
+        self.y_openloop_voltage.hardware_read_func = lambda:  self.ecc100.read_openloop_voltage(Y_AXIS)
+        self.y_openloop_voltage.hardware_set_func  = lambda y: self.ecc100.write_openloop_voltage(Y_AXIS, y)    
+        
+        self.x_frequency.hardware_read_func = lambda:  self.ecc100.read_frequency(X_AXIS)
+        self.x_frequency.hardware_set_func  = lambda x: self.ecc100.write_frequency(X_AXIS, x)   
+        
+        self.y_frequency.hardware_read_func = lambda:  self.ecc100.read_frequency(Y_AXIS)
+        self.y_frequency.hardware_set_func  = lambda y: self.ecc100.write_frequency(Y_AXIS, y)        
+        
+        self.x_actor_info.hardware_read_func = lambda: self.ecc100.read_actor_info(X_AXIS)
+        self.y_actor_info.hardware_read_func = lambda: self.ecc100.read_actor_info(Y_AXIS)
+        
+        
+        
+        
+                
     def disconnect(self):
         
 
