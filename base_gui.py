17bf080fb8067765a48133b41c7267027531fbcd
Edward Barnard <esbarnard@lbl.gov>
2014-07-29 00:12:16 -0700

    Converted TRPL microscope to use a BaseGUI class that contains a common set of functions for any microscope GUI.

---
+++ base_gui.py
@@ -0,0 +1,102 @@
+'''
+Created on Jul 23, 2014
+
+'''
+
+import sys, os
+import time
+import datetime
+import numpy as np
+import collections
+
+from PySide import QtCore, QtGui, QtUiTools
+
+import matplotlib
+matplotlib.rcParams['backend.qt4'] = 'PySide'
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar2
+
+from matplotlib.figure import Figure
+
+from logged_quantity import LoggedQuantity
+
+
+class BaseMicroscopeGUI(object):
+    
+    ui_filename = None
+    
+    def __del__ ( self ): 
+        self.ui = None
+
+    def show(self): 
+        #self.ui.exec_()
+        self.ui.show()
+
+    def __init__(self):
+        self.logged_quantities = collections.OrderedDict()
+        self.hardware_components = collections.OrderedDict()
+        self.measurement_components = collections.OrderedDict()
+        self.figs = collections.OrderedDict()
+
+        # Load Qt UI from .ui file
+        ui_loader = QtUiTools.QUiLoader()
+        ui_file = QtCore.QFile(self.ui_filename)
+        ui_file.open(QtCore.QFile.ReadOnly); 
+        self.ui = ui_loader.load(ui_file)
+        ui_file.close()
+
+        # Run the subclass setup function
+        self.setup()
+
+        # Setup the figures         
+        for name, measure in self.measurement_components.items():
+            print "setting up figures for", name, "measurement", measure.name
+            measure.setup_figure()
+
+
+    def setup(self):
+        """ Override to add Hardware and Measurement Components"""
+        raise NotImplementedError()
+        
+    def add_figure(self,name, widget):
+            """creates a matplotlib figure attaches it to the qwidget specified
+            (widget needs to have a layout set (preferably verticalLayout) 
+            adds a figure to self.figs"""
+            print "---adding figure", name, widget
+            if name in self.figs:
+                return self.figs[name]
+            else:
+                fig = Figure()
+                fig.patch.set_facecolor('w')
+                canvas = FigureCanvas(fig)
+                nav    = NavigationToolbar2(canvas, self.ui)
+                widget.layout().addWidget(canvas)
+                widget.layout().addWidget(nav)
+                canvas.setFocusPolicy( QtCore.Qt.ClickFocus )
+                canvas.setFocus()
+                self.figs[name] = fig
+                return fig
+    
+    def add_logged_quantity(self, name, **kwargs):
+        lq = LoggedQuantity(name=name, **kwargs)
+        self.logged_quantities[name] = lq
+        return lq
+
+    def add_hardware_component(self,hc):
+        self.hardware_components[hc.name] = hc
+        return hc
+    
+    def add_measurement_component(self, measure):
+        assert not measure.name in self.measurement_components.keys()
+        self.measurement_components[measure.name] = measure
+        return measure
+    
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    app.setApplicationName("CL Microscope Control Application")
+    
+    gui = CLMicroscopeGUI()
+    gui.show()
+    
+    sys.exit(app.exec_())
