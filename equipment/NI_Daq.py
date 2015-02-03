b1435a1556369548576e77a79db58b95e2f6de58
Frank Ogletree <dfogletree@lbl.gov>
2015-02-03 12:39:45 -0800

    Added sync class, moved test code to auger_test, created scanner.py for scanning

--- equipment/NI_Daq.py
+++ equipment/NI_Daq.py
@@ -419,9 +419,15 @@ class Sync(NI):
         trig_name = '/' + buff.value + '/ai/StartTrigger'
         self.dac.task.CfgDigEdgeStartTrig(trig_name, mx.DAQmx_Val_Rising)
         
-    def setup(self, rate_out, count_out, rate_in, count_in):
+    def setup(self, rate_out, count_out, rate_in, count_in, pad = True):
+        # Pad = true, acquire one extra input value per channel, strip off
+        # first read, so writes/reads align 
+        if pad:
+            self.delta = np.int(np.rint(rate_in / rate_out))
+        else:
+            self.delta = 0
         self.dac.set_rate(rate_out, count_out, finite=True)
-        self.adc.set_rate(rate_in, count_in)
+        self.adc.set_rate(rate_in, count_in+self.delta)
         
     def out_data(self, data):
         self.dac.load_buffer(data)
@@ -430,163 +436,6 @@ class Sync(NI):
         self.dac.start() #start dac first, waits for trigger from ADC to output data
         self.adc.start()
        
-    def read_buffer(self):
-        return self.adc.read_buffer() 
-    
-#  various test code 
-if __name__ == '__main__':
-    import time
-    import matplotlib.pyplot as plt
-
-    test = 'sync'
-    
-    if test == 'sync':
-        # make output waveform
-        block = 100
-        rate = 1.0e5
-                
-        amplitude = 0.5
-        period = 2
-        out1 = amplitude * np.sin( np.linspace( 0, period*2*np.pi, block) )
-        out2 = amplitude * np.sin( np.linspace( 0 + np.pi/2, period*2*np.pi+ np.pi/2, block) )
-        buff_out = np.zeros( len(out1) + len(out2) )
-        buff_out[::2] = out1    #interlaced output
-        buff_out[1::2] = out2   
-
-        #setup tasks
-        scan = Sync('X-6368/ao0:1', 'X-6368/ai2:3', 1.0)
-        scan.setup(rate, block, 10*rate, 10*block)
-        scan.out_data(buff_out)
-
-        scan.start()
-        x = scan.read_buffer()
-        
-        in1 = x[::2]
-        in2 = x[1::2]
-        plt.subplot(211)
-        plt.plot(in1)
-        plt.plot(out1)
-        plt.subplot(212)
-        plt.plot(in2)
-        plt.plot(out2)
-        plt.show()
-    else:         
-        dac = Dac('X-6368/ao0:1', 'SEM ext scan')
-        data = np.arange(0, 10, 0.001, dtype = np.float64)
-    
-        adc = Adc('X-6368/ai2:3', 5, name = 'chan 2-3')
-    
-    if test == 'dual':
-        # make output waveform
-        block = 100000
-        amplitude = 0.5
-        period = 2
-        out1 = amplitude * np.sin( np.linspace( 0, period*2*np.pi, block) )
-        out2 = amplitude * np.sin( np.linspace( 0 + np.pi/2, period*2*np.pi+ np.pi/2, block) )
-        buff_out = np.zeros( len(out1) + len(out2) )
-        buff_out[::2] = out1    #interlaced output
-        buff_out[1::2] = out2   
-#         plt.plot( out1 )
-#         plt.plot( out2 )
-#         plt.show()
-        
-        rate = 1.0e6
-        adc.set_rate(rate, block)   #finite read
-        dac.set_rate(rate, block, finite=True)
-        
-         
-        buffSize = 512
-        buff = mx.create_string_buffer( buffSize )
-        adc.task.GetNthTaskDevice(1, buff, buffSize)    #DAQmx name for input device
-        trig_name = '/' + buff.value + '/ai/StartTrigger'
-        print 'trigger name' , trig_name 
-        dac.task.CfgDigEdgeStartTrig(trig_name, mx.DAQmx_Val_Rising)
-        dac.load_buffer(buff_out)
-        dac.start() #start dac first, waits for trigger from ADC to output data
-        adc.start()
-
-        x = adc.read_buffer()
-        in1 = x[::2]
-        in2 = x[1::2]
-        plt.subplot(211)
-        plt.plot(in1)
-        plt.plot(out1)
-        plt.subplot(212)
-        plt.plot(in2)
-        plt.plot(out2)
-        plt.show()
-        
-    if test == 'read block':
-        adc.set_rate( 1e5, 1000 )   #finite read
-        x = adc.read_buffer()
-        #plt.ion()
-        plt.subplot(211)
-        plt.plot(x[::2])
-        plt.subplot(212)
-        plt.plot(x[1::2])
-        plt.show()
-        #raw_input("press enter")
-        for i in range(5):
-            x = adc.read_buffer()
-            plt.subplot(211)
-            plt.plot(x[::2])
-            plt.subplot(212)
-            plt.plot(x[1::2])
-            plt.show()      
-            #raw_input("press enter")
-            
-    if test == 'read single':
-        dac.set((0.5,-1))
-        for i in range(16): 
-            data = adc.get()
-            print 'cycle {}, {}, {}'.format( i, data[0], data[1] )
-        count = 10000
-        #adc.set_single_read()
-        adc.start()
-        elapsed = time.clock()
-        for i in range(count):
-            adc.get()
-        elapsed = time.clock() - elapsed
-        print 'read {} single samples @ {} us per sample'.format( count, 1e6*elapsed/count )       
-        
-                
-    if test == 'start stop' or test == 'all':
-        dac.set_rate(2e6, len(data)/2, finite=True)
-        dac.load_buffer(data)
-        dac.ready()
-        elapsed = time.clock()
-        for i in range(16):
-            dac.start()
-            dac.wait()
-            dac.stop()
-        elapsed = time.clock() - elapsed
-        print 'output buffer 16 times of {} 2-channel points in {} sec'.format( dac._sample_count, elapsed )
-        
-    if test == 'single' or test == 'all':
-        data0 = np.zeros(2, dtype=np.float64)
-        data1 = np.zeros(2, dtype=np.float64)
-        data0[0] = data1[1] = 0.5
-        count = 10000
-
-        #dac.set_single()
-        dac.start()
-        elapsed = time.clock()
-        for i in range(count):
-            dac.set(data0)
-            dac.set(data1)
-        elapsed = time.clock() - elapsed
-        print 'wrote {} single samples @ {} us per sample'.format( 2*count, 1e6*elapsed/(2*count) )
-        
-    if test == 'auto' or test == 'all':
-        dac.set_rate(2e6, len(data)/2, finite=True)
-        elapsed = time.clock()
-        for i in range(16):
-            dac.stop()
-            dac.load_buffer(data, auto=True)   
-            #dac.start()
-            dac.wait()
-            #dac.stop()
-        elapsed = time.clock() - elapsed
-        print 'wrote 16 autostart buffers of {} 2-channel points in {} sec'.format( dac._sample_count, elapsed )
-
-print 'done'
+    def read_buffer(self, timeout = 1.0):
+        x = self.adc.read_buffer(timeout=timeout)
+        return x[self.delta*self.adc.get_chan_count()::] 
