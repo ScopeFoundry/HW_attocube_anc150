e9bd3124549ee25a7307ba18f9c5d842ba3311fc
Frank Ogletree <dfogletree@lbl.gov>
2015-02-12 17:03:29 -0800

    -implemented scan saving functionality, currently only save dataset, but not metadata
    -Hao

--- equipment/image_io.py
+++ equipment/image_io.py
@@ -2,100 +2,137 @@ import h5py
 import numpy as np
 
 class ChannelInfo(object):
+    
     def __init__(self,
                  name='channel0',
-                 dimension=(1024,1024),
+                 dimension=(3,3),
                  dtype=np.float,
                  unit=''):
         self.name=name
         self.dimension=dimension
         self.dtype=dtype
         self.unit=unit
-
+        
+class Channel(object):
+    '''
+    Channel class holds all the channel information,
+    as well as link to the the channel data
+    '''
+    def __init__(self,
+                 channel_info,
+                 data_group,
+                 initial_size=100):
+         
+        self.name=channel_info.name
+        self.dimension=channel_info.dimension
+        self.dtype=channel_info.dtype
+        self.unit=channel_info.unit
+        self.data_group=data_group
+        self.size=initial_size
+        self.setup()
+         
+    def setup(self):
+        self.data=self.data_group.create_dataset(self.name,
+                                                (self.size,)+self.dimension,
+                                                maxshape=(None,)+self.dimension,
+                                                dtype=self.dtype)
+             
+    def resize(self,size):
+        self.size=size
+        self.data.resize(self.size,0)
+     
+    def add_frame(self,counter,frame):
+        self.data[counter]=frame        
+ 
+     
+ 
 class Collection(object):
     '''
     Collection object handle the data storage of a scan
     it store all the attributes in a 
     '''
-
+ 
     def __init__(self,
                  name='collection0',
                  create=True,
-                 initial_size=1000,
-                 expansion_size=1000, 
-                 channel_info=[ChannelInfo()]):
+                 initial_size=100,
+                 expansion_size=100, 
+                 channel_infos=[ChannelInfo(),]):
         if create:
             self.name=name
             self.initial_size=initial_size
             self.expansion_size=expansion_size
             self.size=self.initial_size
-            self.channel_info=channel_info
+             
+            self.create()
+            self.setup_main_group()
+            self.counter=0
+             
+            '''
+            create a list and setup the channels
+            '''
+            self.channel_infos=channel_infos
+            self.setup_channels(self.channel_infos)
         else:
             pass
-        
+         
     def create(self):
         try:
             self.file=h5py.File(self.name+'.hdf5','w')
         except:
             raise IOError('The file name already exist, please choose a new name')
-            
+             
     def close(self):
         self.file.close()
-        
+         
     def setup_main_group(self):
         self.measurement=self.file.create_group('measurement')
         self.hardware=self.file.create_group('hardware')
         self.data_group=self.file.create_group('data_group')
-        
-    
-    def setup_channel(self,cannel_info=ChannelInfo()):
+         
+     
+    def setup_channels(self,channel_infos=[ChannelInfo()]):
         '''
-        create a new channel in the data_group
+        create a list of channels and set them up
         '''
-    
-    def create_channel(self,channel_info=ChannelInfo()): 
+        self.channels=list()
+         
         '''
-        DummyHolder for class Channel
+        for all channel listed in channel_infos,
+        create a channel object and add it to the channel list
         '''
-        parent=self
-        
-        class Channel(object):
-        
-            def __init__(self,channel_info=ChannelInfo()):
-                self.name=channel_info.name
-                self.dimension=channel_info.dimension
-                self.dtype=channel_info.dtype
-                self.unit=channel_info.unit
-                self.parent=parent
-                
-        
-            def setup(self):
-                pass
+        for channel_info in channel_infos:
+            self.channels.append(Channel(channel_info,
+                                         data_group=self.data_group,
+                                         initial_size=self.initial_size))
+   
+    def add_frames(self,counter,frames):
+        '''
+        read frames from input and write them
+        into the dataset of each channel
+        frame is a dictionary
+        '''
+        #check to see if counter have reached the size of arrays
+        if self.counter>=self.size:
+            self.expand()
              
-          
-    def link(self,group,counter,data):
-        self.group=group
-        self.counter=counter
-        self.data=data
-        
-    def data_unlinked(self):
-        if self.data==None:
-            return True
-        else:
-            return False
-        
-    def counter_unlinked(self):
-        if self.counter==None:
-            return True
-        else:
-            return False
-    
-    def group_unlinked(self):
-        if self.counter==None:
-            return True
-        else:
-            return False
-    
-    def add_frame(self,frame,count=None):
-        if count==None:
-            count=len
+        for channel in self.channels:
+            channel.add_frame(self.counter,frames[channel.name])
+             
+        #increase the count by 1    
+        self.counter+=1
+         
+    def update(self,frames):
+        self.add_frames(self.counter,frames)
+                    
+    def expand(self):
+        '''
+        increase the size of collection datasets by expansion_size,
+        iterate through all channels
+        '''
+        self.size+=self.expansion_size
+        for channel in self.channels:
+            channel.resize(self.size)
+ 
+if __name__=='main':
+    print('Start testing:')
---
+++ equipment/image_io_test.py
@@ -0,0 +1,27 @@
+'''
+Created on Feb 12, 2015
+
+@author: NIuser
+'''
+from image_io import ChannelInfo
+from image_io import Collection
+import numpy as np
+ 
+ 
+channel_info1=ChannelInfo('random',(3,3))
+channel_info2=ChannelInfo('zeros',(5,5))
+ch_infos=[channel_info1,channel_info2]
+print(ch_infos)
+     
+test_collection=Collection(name='test_collection',
+                           create=True,
+                           initial_size=100,
+                           expansion_size=100, 
+                           channel_infos=ch_infos)
+print(test_collection)
+      
+for i in range(950):
+    ch1=np.random.rand(3,3)
+    ch2=np.zeros((5,5))
+    frames={'random':ch1,'zeros':ch2}
+    test_collection.update(frames)
