cea73b8c211774a764d517d1cf93149511e1589f
Edward Barnard <esbarnard@lbl.gov>
2015-08-09 14:38:36 -0700

    single_particle_blink now uses tttr modes of picoharp

--- measurement_components/single_particle_blink.py
+++ measurement_components/single_particle_blink.py
@@ -33,48 +33,48 @@ class SingleParticleBlink(Measurement):
             ## acquire Spec
             self.gui.andor_ro_measure.read_single.update_value(True)
             self.gui.andor_ro_measure._run()
-            self.save_dict['wls'] = self.gui.andor_ro_measure.wls.copy()
-            self.save_dict['spectrum_before'] = self.gui.andor_ro_measure.spectrum.copy()
+            #self.save_dict['wls'] = self.gui.andor_ro_measure.wls.copy()
+           # self.save_dict['spectrum_before'] = self.gui.andor_ro_measure.spectrum.copy()
             ## Close shutter
             self.close_shutter()
     
             # Collect lifetime
-            print "lifetime before"
+            print "T3 measurement"
             ## Flip mirror to APD
             self.flip_to_apd()
             ## Open shutter
             self.open_shutter()
             ## acquire picoharp
-            self.gui.picoharp_measure._run()
-            self.save_dict['ph_time_histogram_before'] = self.gui.picoharp_hc.picoharp.histogram_data.copy() # Make sure it copies!
-            self.save_dict['ph_time_array_before'] = self.gui.picoharp_hc.picoharp.time_array.copy() # Make sure it copies!
-            self.save_dict['ph_elapsed_meas_time'] = self.gui.picoharp_hc.picoharp.read_elapsed_meas_time()
+            self.gui.picoharp_tttr_measure._run()
+           # self.save_dict['ph_time_histogram_before'] = self.gui.picoharp_hc.picoharp.histogram_data.copy() # Make sure it copies!
+            #self.save_dict['ph_time_array_before'] = self.gui.picoharp_hc.picoharp.time_array.copy() # Make sure it copies!
+            #self.save_dict['ph_elapsed_meas_time'] = self.gui.picoharp_hc.picoharp.read_elapsed_meas_time()
             ## Close shutter
             self.close_shutter()
             
             # Collect apd_optimzer (look for blinking)
-            print "apd_trace"
+            #print "apd_trace"
             ## Open shutter
-            self.open_shutter()
+            #self.open_shutter()
             ## acquire apd_optimizer
-            self.apd_trace = [] 
-            self.save_dict['apd_trace'] =self.apd_trace
-            self.apd_trace_time = []
-            self.save_dict['apd_trace_time'] =self.apd_trace_time
-            self.t0 = time.time()
-            self.apd_trace_elapsed = 0
-            print self.apd_trace_max_time.val
-            while self.apd_trace_elapsed < self.apd_trace_max_time.val:
-                if self.interrupt_measurement_called:
-                    break
-                self.gui.apd_counter_hc.apd_count_rate.read_from_hardware()            
-                self.apd_trace.append(self.gui.apd_counter_hc.apd_count_rate.val  )
-                self.apd_trace_time.append(self.apd_trace_elapsed)
-                self.apd_trace_elapsed = time.time() - self.t0
+            #self.apd_trace = [] 
+            #self.save_dict['apd_trace'] =self.apd_trace
+            #self.apd_trace_time = []
+            #self.save_dict['apd_trace_time'] =self.apd_trace_time
+            #self.t0 = time.time()
+            #self.apd_trace_elapsed = 0
+            #print self.apd_trace_max_time.val
+            #while self.apd_trace_elapsed < self.apd_trace_max_time.val:
+            #    if self.interrupt_measurement_called:
+            #        break
+            #    self.gui.apd_counter_hc.apd_count_rate.read_from_hardware()            
+            #    self.apd_trace.append(self.gui.apd_counter_hc.apd_count_rate.val  )
+            #    self.apd_trace_time.append(self.apd_trace_elapsed)
+            #    self.apd_trace_elapsed = time.time() - self.t0
                 #print self.apd_trace_elapsed
             ## close shutter
-            self.close_shutter()
-            print "apd_trace done"
+            #self.close_shutter()
+            #print "apd_trace done"
             
             # Collect Spectrum
             print "spectrum_after"
@@ -90,18 +90,18 @@ class SingleParticleBlink(Measurement):
             self.close_shutter()
             
             # Collect lifetime
-            print "lifetime after"
+            #print "lifetime after"
             ## Flip mirror to APD
-            self.flip_to_apd()
+            #self.flip_to_apd()
             ## Open shutter
-            self.open_shutter()
+            #self.open_shutter()
             ## acquire picoharp
-            self.gui.picoharp_measure._run()
-            self.save_dict['ph_time_histogram_after'] = self.gui.picoharp_hc.picoharp.histogram_data.copy()
-            self.save_dict['ph_time_array_after'] = self.gui.picoharp_hc.picoharp.time_array.copy()
-            self.save_dict['ph_elapsed_meas_time_after'] = self.gui.picoharp_hc.picoharp.read_elapsed_meas_time()        
+            #self.gui.picoharp_measure._run()
+            #self.save_dict['ph_time_histogram_after'] = self.gui.picoharp_hc.picoharp.histogram_data.copy()
+            #self.save_dict['ph_time_array_after'] = self.gui.picoharp_hc.picoharp.time_array.copy()
+            #self.save_dict['ph_elapsed_meas_time_after'] = self.gui.picoharp_hc.picoharp.read_elapsed_meas_time()        
             ## Close shutter
-            self.close_shutter()
+            #self.close_shutter()
         
         finally:
             for lqname,lq in self.gui.logged_quantities.items():
@@ -153,18 +153,21 @@ class SingleParticleBlinkSet(Measurement):
     def _run(self):
 
         self.coords = [
-         (0.666630001357 ,9.47510785147, None),
-        # (1.29834601844 ,7.72092102085, None),
-        # (0.961604774384 ,5.98109125988, None),
-        # (1.6350872625 ,3.90452025485, None),
-        # (3.76778180821 ,2.38918465658, None),
-        # (6.01272343527 ,4.35350858026, None),
-        # (7.02294716745 ,2.38918465658, None),
-        # (7.52805903354 ,5.47597939379, None),
-        # (8.25766506233 ,8.00153872423, None),
-        # (9.38013587586 ,3.0626671447, None),
-        # (5.33924094715 ,0.537107814255, None),
-        # (3.65553472686 ,0.761601976961, None),
+(41.2425500007 ,58.0088831721, None),
+(44.6808999546 ,56.2227273519, None),
+(48.1639038039 ,56.2673812474, None),
+(50.9324453253 ,55.6868806059, None),
+(51.7362154443 ,52.9183390846, None),
+(54.4601030702 ,52.695069607, None),
+(57.0500290094 ,53.0076468756, None),
+(55.3531809802 ,56.401342934, None),
+(56.8267595319 ,54.9724182778, None),
+(57.630529651 ,46.8900631914, None),
+(44.9934772231 ,45.9969852813, None),
+(43.6985142535 ,43.585674924, None),
+(51.1110609073 ,41.7102113128, None),
+(56.3802205769 ,41.3083262533, None),
+(41.8677045377 ,48.6315651161, None),
         ]
         
 
@@ -179,6 +182,13 @@ class SingleParticleBlinkSet(Measurement):
             
             self.gui.single_particle_blink_measure.interrupt_measurement_called=False
             self.gui.single_particle_blink_measure._run()
+            
+        # rerun apd map
+        self.gui.flip_mirror_hc.flip_mirror_position.update_value(
+                                          self.gui.flip_mirror_hc.POSITION_APD)
+        self.gui.apd_scan_measure.interrupt_measurement_called=False
+        self.gui.apd_scan_measure._run()
+            
         print self.name, "done"
             
     def update_display(self):
