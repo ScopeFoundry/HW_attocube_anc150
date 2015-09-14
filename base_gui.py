d8811e1debbe6655f9b1c09e7b699d6e6eed00c9
Edward Barnard <esbarnard@lbl.gov>
2015-09-13 22:20:27 -0700

    base_gui: Close on main window confirms about quitting application

--- base_gui.py
+++ base_gui.py
@@ -47,9 +47,12 @@ class BaseMicroscopeGUI(object):
         # Load Qt UI from .ui file
         ui_loader = QtUiTools.QUiLoader()
         ui_file = QtCore.QFile(self.ui_filename)
-        ui_file.open(QtCore.QFile.ReadOnly); 
+        ui_file.open(QtCore.QFile.ReadOnly)
         self.ui = ui_loader.load(ui_file)
         ui_file.close()
+        
+        self.closeEventEater = CloseEventEater()
+        self.ui.installEventFilter(self.closeEventEater)
 
         # Run the subclass setup function
         self.setup()
@@ -231,6 +234,25 @@ class BaseMicroscopeGUI(object):
     
     def settings_load_dialog(self):
         pass
+    
+class CloseEventEater(QtCore.QObject):
+    def eventFilter(self, obj, event):
+        if event.type() == QtCore.QEvent.Close:
+            # eat close event
+            print "close"
+            reply = QtGui.QMessageBox.question(None, 
+                                               "Close ScopeFoundry?", 
+                                               "Do you wish to shut down ScopeFoundry?",
+                                               QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+            if reply == QtGui.QMessageBox.Yes:
+                QtGui.QApplication.quit()
+                event.accept()
+            else:
+                event.ignore()
+            return True
+        else:
+            # standard event processing            
+            return QtCore.QObject.eventFilter(self,obj, event)
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
