9c4050bb036c9b8bbadd688b16d58d9cf12e5230
Zach Anderson <andersonzacharyt@gmail.com>
2015-10-14 14:45:02 -0700

    em_series_run: initial commit, semi-stable

---
+++ em_series_run.py
@@ -0,0 +1,36 @@
+import sys
+from PySide import QtGui
+
+from base_gui import BaseMicroscopeGUI
+
+# Import Hardware Components
+from hardware_components.em_hardware import EMHardwareComponent
+
+# Import Measurement Components
+from measurement_components.em_series import SeriesMeasurement
+
+class EM_Acquisition(BaseMicroscopeGUI):
+
+    ui_filename = "base_gui.ui"
+
+    def setup(self):
+        #Add hardware components
+        print "Adding Hardware Components"
+        self.add_hardware_component(EMHardwareComponent(self))
+
+        #Add measurement components
+        print "Create Measurement objects"
+        self.add_measurement_component(SeriesMeasurement(self))
+
+        
+        #set some default logged quantities
+        self.hardware_components['em_acquirer'].debug_mode.update_value(True)
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    app.setApplicationName("EM Series Tool")
+
+    gui = EM_Acquisition(app)
+    gui.show()
+
+    sys.exit(app.exec_())
