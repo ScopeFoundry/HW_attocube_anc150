ad14f17d43322375945a8b8c520d97b76daf4d06
Zach Anderson <andersonzacharyt@gmail.com>
2015-10-21 13:22:22 -0700

    tem_hardware: initial commit post-split

--- hardware_components/tem_hardware.py
+++ hardware_components/tem_hardware.py
@@ -6,12 +6,11 @@ try:
 except Exception as err:
     print "Cannot load required modules for em_hardware:", err
 
-
-class EMHardwareComponent(HardwareComponent):
+class TEMHardwareComponent(HardwareComponent):
 
     def setup(self):
-        self.debug = False
-        self.name = "em_acquirer"
+        self.debug = True
+        self.name = "tem_hardware"
 
         # Create logged quantities
         self.current_defocus = self.add_logged_quantity(
@@ -29,15 +28,6 @@ class EMHardwareComponent(HardwareComponent):
                                 dtype = float, fmt="%e", ro=True,
                                 unit="s",
                                 vmin=0.01,vmax=100.0)
-        self.current_dwell = self.add_logged_quantity(
-                                name = 'current_dwell',
-                                dtype = float, fmt="%e", ro=True,
-                                unit="ns",
-                                vmin=1e-9,vmax=1000000.0)
-        self.mode = self.add_logged_quantity(
-                                name = 'mode',
-                                dtype = str, fmt="%s", ro=False,
-                                unit=None)
 
         self.dummy_mode = self.add_logged_quantity(name='dummy_mode',
                             dtype=bool, initial=False, ro=False)
@@ -53,52 +43,36 @@ class EMHardwareComponent(HardwareComponent):
             if self.debug_mode.val: print "Connecting to Scope"
             self.wrapper = NCEMscope.ScopeWrapper(debug = self.debug_mode.val, mode='STEM')
             self.wrapper.Connect()
+            self.wrapper.TEMMODE() 
+            
+            #handy to have these references
             self.Scope = self.wrapper.Scope
             self.Acq = self.wrapper.Acq
             self.Ill = self.wrapper.Ill
             self.Proj = self.wrapper.Proj
+            self.Stage = self.wrapper.Stage
              
-            self.mode.update_value(new_val=self.wrapper.mode, update_hardware=False)
+            self.current_defocus.hardware_read_func = \
+                self.wrapper.getDefocus
+            self.current_defocus.hardware_set_func = \
+                self.wrapper.setDefocus
+                
             self.current_binning.hardware_read_func = \
                 self.wrapper.getBinning
             self.current_binning.hardware_set_func = \
                 self.wrapper.setBinning
+
             self.current_exposure.hardware_read_func = \
                 self.wrapper.getExposure
             self.current_exposure.hardware_set_func = \
                 self.wrapper.setExposure
-            self.current_dwell.hardware_read_func = \
-                self.wrapper.getDwellTime
-            self.current_dwell.hardware_set_func = \
-                self.wrapper.setDwellTime
             
         else:
             if self.debug_mode.val: print "Doing nothing (Dummy Mode)"
 
-        # connect logged quantities
-
-    def setup4Stem(self):
-        self.wrapper.STEMMODE()
-        print '-----set up for stem-----'
-    def setup4Tem(self):
-        self.wrapper.TEMMODE()
-        print '-----set up for tem-----'
     def acquire(self):
         return self.wrapper.Acq.AcquireImages()
-    def setTemAcqVals(self,binning,exposure):
-        myCcdAcqParams = self.wrapper.Cam.AcqParams
-        myCcdAcqParams.Binning = int(binning)
-        myCcdAcqParams.ExposureTime = float(exposure)
-        myCcdAcqParams.ImageCorrection = self.wrapper.ACQIMAGECORRECTION_UNPROCESSED #this has to be unprocessed. Not sure if it affects data from the micoscope itself
-        myCcdAcqParams.ImageSize = self.wrapper.ACQIMAGESIZE_FULL  
-        self.wrapper.Cam.AcqParams = myCcdAcqParams
-        print '-----set TEM vals-----'
-    def setStemAcqVals(self,binning,dwell):
-        self.myStemAcqParams = self.Acq.Detectors.AcqParams
-        self.myStemAcqParams.Binning = int(binning)
-        self.myStemAcqParams.DwellTime = float(dwell)
-        self.Acq.Detectors.AcqParams = self.myStemAcqParams
-        print '-----set STEM vals-----'
+
     def disconnect(self):
         #disconnect logged quantities from hardware
         for lq in self.logged_quantities.values():
