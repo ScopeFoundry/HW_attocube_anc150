8838155ae86ca303597667a333af11fe075cb6d3
Zach Anderson <andersonzacharyt@gmail.com>
2015-10-27 04:46:00 -0700

    em_tomo_series: first big overhaul, added preview, connected LQs to UI, implemented minTilt/maxTilt, cleaned processes

--- measurement_components/em_tomo_series.py
+++ measurement_components/em_tomo_series.py
@@ -1,40 +1,61 @@
 from measurement import Measurement
-from numpy import ndarray,zeros,uint16,array
+from numpy import ndarray,zeros,uint16,array, round
 import pyqtgraph as pg
 import pythoncom
 from PySide import QtCore, QtGui
 import win32com.client
+from random import choice, random, randint
+
 class EMTomographySeries(Measurement):
     itr_finished = QtCore.Signal(ndarray)
     name = "em_tomography"
     ui_filename = "measurement_components/em_tomo.ui"
     
-    def setup(self):        
+    def setup(self):
         self.display_update_period = 0.1 #seconds
-        self.debug = True
+        self.debug = True       
+        self.getHardware()
+        self.setupUI()
+
+    def getHardware(self):
         self.hardware = self.gui.hardware_components['em_hardware']
         self.hardware.connect()
+        self.hardware.debug_mode.update_value(new_val=True)
         self._id = pythoncom.CoMarshalInterThreadInterfaceInStream(pythoncom.IID_IDispatch,self.hardware.Scope)
 
-        try:
-            self.ui.btnAcq.released.connect(self.start)
-            self.ui.btnAbo.released.connect(self.interrupt)   
-            self.ui.btnMinTilt.released.connect(self.minTilt)     
-            self.ui.btnMaxTilt.released.connect(self.maxTilt)  
-            self.measurement_sucessfully_completed.connect(self.postAcquisition)
-            self.itr_finished[ndarray].connect(self.postIteration)
-            
-            self.minimum_tilt = self.add_logged_quantity(
+    def setupUI(self):
+        
+        self.minimum_tilt = self.add_logged_quantity(
                                 name = 'minimum_tilt',
                                 dtype = float, fmt="%e", ro=False,
                                 unit='deg', vmin=-80.0,vmax=80.0)
-            self.maximum_tilt = self.add_logged_quantity(
+        self.maximum_tilt = self.add_logged_quantity(
                                 name = 'maximum_tilt',
                                 dtype = float, fmt="%e", ro=False,
                                 unit='deg', vmin=-80.0,vmax=80.0)
-            self.minimum_tilt.connect_bidir_to_widget(self.gui.ui.minBox)
-            self.maximum_tilt.connect_bidir_to_widget(self.gui.ui.maxBox)
-        except Exception as err:
+        try:
+            self.ui.setWindowTitle('NCEM Tomography Tool')
+            self.ui.btnPre.released.connect(self.preview)
+            self.ui.btnAcq.released.connect(self.start)
+            self.ui.btnAbo.released.connect(self.interrupt)   
+            self.ui.btnMinTilt.released.connect(self.minTilt)     
+            self.ui.btnMaxTilt.released.connect(self.maxTilt)
+            #bin stuff
+            self.ui.buttonGroup.setId(self.ui.bin1,1)
+            self.ui.buttonGroup.setId(self.ui.bin2,2)
+            self.ui.buttonGroup.setId(self.ui.bin4,4)  
+            self.ui.buttonGroup.buttonReleased[int].connect(self.binChanged)
+            self.ui.buttonGroup.button(self.hardware.current_binning.val).setChecked(True)
+            
+            self.hardware.current_exposure.connect_bidir_to_widget(self.ui.expBox)
+            self.hardware.current_defocus.connect_bidir_to_widget(self.ui.defBox)
+            
+            self.measurement_sucessfully_completed.connect(self.postAcquisition)
+            self.itr_finished[ndarray].connect(self.postIteration)
+            
+#             self.minimum_tilt.connect_bidir_to_widget(self.gui.ui.minBox)
+#             self.maximum_tilt.connect_bidir_to_widget(self.gui.ui.maxBox)
+        except Exception as err: 
             print "EM_Tomography: could not connect to custom main GUI", err
     def getScope(self):
         self._m = win32com.client.Dispatch(pythoncom.CoGetInterfaceAndReleaseStream(self._id, 
@@ -42,48 +63,78 @@ class EMTomographySeries(Measurement):
         self.Acq = self._m.Acquisition
         self.Proj = self._m.Projection
         self.initialDF = self.Proj.Defocus
-        print '-----DF:', str(self.initialDF),'m-----'
-        self.setTemAcqVals()
-        print '-----end of getscope-----'
+        if self.debug: print '-----DF:', str(self.initialDF),'m-----'
+        if self.debug: print '-----end of getscope-----'
+    def dummyStuff(self):
+        x = round(random(),2)
+        y = round(random(),2)
+        lblx = choice(['+','-'])
+        lbly = choice(['+','-'])
+        if x%2==1: lbl = '+'
+        self.ui.lblXYShift.setText('('+lblx+str(x)+', '+lbly+str(y)+')')
+        d = randint(0,6)
+        lbld = choice(['+','-'])
+        if d==0: lbld=''
+        self.ui.lblDeltaDefocus.setText(lbld+str(d)+'nm')
+    def allocateStorage(self):
+        if self.debug: print '-----allocating arrays-----'
+        deltas = len(self.tiltList)
+        self.series = zeros([self.xRes,self.yRes,deltas],dtype=uint16,) #contains raw data
     def setup_figure(self):
         if hasattr(self, 'graph_layout'):
             self.graph_layout.deleteLater() # see http://stackoverflow.com/questions/9899409/pyside-removing-a-widget-from-a-layout
             del self.graph_layout
         self.graph_layout=pg.GraphicsLayoutWidget(border=(100,100,100))
         self.ui.plot_groupBox.layout().addWidget(self.graph_layout)
-        ## Add 3 plots into the first row (automatic position)
         self.viewer = self.graph_layout.addViewBox()
         self.viewer.enableAutoRange()
-    def _run(self):
+    def acqFoc(self):
+        for ii in range(len(self.tiltList)):
+            tiltVal = float(self.tiltList[ii])
+            self.hardware.setAlphaTilt(tiltVal)
+            acquiredImageSet = self.Acq.AcquireImages()      
+            self.series[:,:,ii] = array(acquiredImageSet(0).AsSafeArray)
+            if self.debug: print '-----acquired @ '+str(self.tiltList[ii])+'deg-----' 
+            if self.debug: print 'Size:'+str(self.series.nbytes)  
+            self.itr_finished.emit(self.series[:,:,ii])
+    def preview(self):
         try:
             if not hasattr(self,'_m') or self._m == None: self.getScope()
             acquiredImageSet = self.Acq.AcquireImages()      
-            something = array(acquiredImageSet(0).AsSafeArray) 
-            self.itr_finished.emit(something)
+            itr = array(acquiredImageSet(0).AsSafeArray) 
+            self.itr_finished.emit(itr)
+        except Exception as err:
+            print self.name, "error:", err
+    def _run(self):
+        try:
+            if not hasattr(self,'_m') or self._m == None: self.getScope()
+            self.allocateStorage()
+            #self.acqFoc()
         except Exception as err:
             print self.name, "error:", err
         #there seems to be no need to emit a finished signal...
-    def setTemAcqVals(self,):
-        myCcdAcqParams = self.Acq.Cameras(0).AcqParams
-        #myCcdAcqParams.Binning = 4
-        #myCcdAcqParams.ExposureTime = 0.1
-        myCcdAcqParams.ImageCorrection = win32com.client.constants.AcqImageCorrection_Unprocessed #this has to be unprocessed. Not sure if it affects data from the micoscope itself
-        myCcdAcqParams.ImageSize = win32com.client.constants.AcqImageSize_Full
-        self.Acq.Cameras(0).AcqParams = myCcdAcqParams
-        print '-----set TEM vals-----'
     def postAcquisition(self):
         print 'postacq'
     def postIteration(self,data):
+        self.viewer.clear()
         self.viewer.addItem(pg.ImageItem(data))
-
+        self.dummyStuff()
         print 'shape:', data.shape
         print 'postiter'
     def minTilt(self):
-        self.hardware.tiltAlpha(0)
+        self.hardware.tiltAlpha(self.hardware.minimum_tilt.val)
         print 'min tilt'
     def maxTilt(self):
-        self.hardware.tiltAlpha(0)
+        self.hardware.tiltAlpha(self.hardware.maximum_tilt.val)
         print 'max tilt'
+    def binChanged(self,btnId):
+        if btnId in self.hardware.getBinnings():
+            res = 2048/btnId
+            self.xRes = res
+            self.yRes = res
+            self.ui.lblXYRes.setText('('+str(res)+", "+str(res)+')')
+            self.hardware.current_binning.update_value(new_val=btnId)
+
     def update_display(self):        
         self.gui.app.processEvents()
 
