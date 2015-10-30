f6a990b58f5d292dcc0589dc6c4239228a391062
Zach Anderson <andersonzacharyt@gmail.com>
2015-10-30 16:51:26 -0700

    em_tomo_series: added __init__, internalized setting CCDAcqParams, working multiple acq + preview from dif. threads

--- measurement_components/em_tomo_series.py
+++ measurement_components/em_tomo_series.py
@@ -2,26 +2,27 @@ from measurement import Measurement
 from numpy import ndarray,zeros,uint16,array, round
 import pyqtgraph as pg
 import pythoncom
-from PySide import QtCore, QtGui
+from PySide import QtCore
 import win32com.client
 from random import choice, random, randint
 from foundry_scope.logged_quantity import LQRange
+from threading import Thread
 
 class EMTomographySeries(Measurement):
     itr_finished = QtCore.Signal(ndarray)
     name = "em_tomography"
     ui_filename = "measurement_components/em_tomo.ui"
-    
+    def __init__(self,gui):
+        self.debug = True
+        Measurement.__init__(self, gui)
+         
     def setup(self):
         self.display_update_period = 0.1 #seconds
-        self.debug = True       
         self.getHardware()
         self.setupUI()
 
     def getHardware(self):
         self.hardware = self.gui.hardware_components['em_hardware']
-        self.hardware.connect()
-        self.hardware.debug_mode.update_value(new_val=True)
         self._id = pythoncom.CoMarshalInterThreadInterfaceInStream(pythoncom.IID_IDispatch,self.hardware.Scope)
 
     def setupUI(self):
@@ -84,6 +85,12 @@ class EMTomographySeries(Measurement):
         self.Acq = self._m.Acquisition
         self.Proj = self._m.Projection
         self.initialDF = self.Proj.Defocus
+        myCcdAcqParams = self.Acq.Cameras(0).AcqParams
+        myCcdAcqParams.Binning = self.hardware.current_binning.val
+        myCcdAcqParams.ExposureTime = self.hardware.current_exposure.val
+        myCcdAcqParams.ImageCorrection = win32com.client.constants.AcqImageCorrection_Unprocessed #this has to be unprocessed. Not sure if it affects data from the micoscope itself
+        myCcdAcqParams.ImageSize = win32com.client.constants.AcqImageSize_Full
+        self.Acq.Cameras(0).AcqParams = myCcdAcqParams
         if self.debug: print '-----DF:', str(self.initialDF),'m-----'
         if self.debug: print '-----end of getscope-----'
     def dummyStuff(self):
@@ -100,7 +107,9 @@ class EMTomographySeries(Measurement):
     def allocateStorage(self):
         if self.debug: print '-----allocating arrays-----'
         deltas = len(self.tiltLQRange.array)
-        self.series = zeros([self.xRes,self.yRes,deltas],dtype=uint16,) #contains raw data
+        self.seriesDict = dict()
+        for x in self.tiltLQRange.array:
+            self.seriesDict.update(x=[])
     def setup_figure(self):
         if hasattr(self, 'graph_layout'):
             self.graph_layout.deleteLater() # see http://stackoverflow.com/questions/9899409/pyside-removing-a-widget-from-a-layout
@@ -110,19 +119,12 @@ class EMTomographySeries(Measurement):
         self.viewer = self.graph_layout.addViewBox()
         self.viewer.enableAutoRange()
     def acqFoc(self):
-        for ii in range(len(self.tiltLQRange.array)):
-            tiltVal = float(self.tiltLQRange.array[ii])
-            self.hardware.setAlphaTilt(tiltVal)
-            acquiredImageSet = self.Acq.AcquireImages()      
-            self.series[:,:,ii] = array(acquiredImageSet(0).AsSafeArray)
-            if self.debug: print '-----acquired @ '+str(tiltVal)+'deg-----' 
-            if self.debug: print 'Size:'+str(self.series.nbytes)  
-            self.itr_finished.emit(self.series[:,:,ii])
+        pass
     def preview(self):
         try:
             print self.tiltLQRange.array
-            if not hasattr(self,'_m') or self._m == None: self.getScope()
-            acquiredImageSet = self.Acq.AcquireImages()      
+            #self.getScope()
+            acquiredImageSet = self.hardware.acquire()      
             itr = array(acquiredImageSet(0).AsSafeArray) 
             self.itr_finished.emit(itr)
         except Exception as err:
@@ -131,17 +133,34 @@ class EMTomographySeries(Measurement):
         try:
             if not hasattr(self,'_m') or self._m == None: self.getScope()
             self.allocateStorage()
-            self.acqFoc()
+            for ii in range(len(self.tiltLQRange.array)):
+                print len(self.tiltLQRange.array)
+                print 'loop ' + str(ii)
+                tiltVal = float(self.tiltLQRange.array[ii])
+                #self.hardware.setAlphaTilt(tiltVal)
+                acquiredImageSet = self.Acq.AcquireImages()      
+                itr = array(acquiredImageSet(0).AsSafeArray) 
+                self.itr_finished.emit(itr)
+                if self.debug: print '-----acquired @ '+str(tiltVal)+'deg-----' 
+                if self.debug: print 'Size:'+str(itr.nbytes)  
+            self.measurement_sucessfully_completed.emit()
         except Exception as err:
             print self.name, "error:", err
     def postAcquisition(self):
         print '-----postacq-----'
-    def postIteration(self,data):
+    def updateView(self,data):
         self.viewer.clear()
         self.viewer.addItem(pg.ImageItem(data))
         self.dummyStuff()
+        self.update_display()
         print 'shape:', data.shape
         print '-----postiter-----'
+    def postIteration(self,data):
+#         thread = Thread(target = self.updateView,args = (data,))
+#         thread.start()
+#         thread.join()
+        self.updateView(data)
+
     def minTilt(self):
         self.hardware.setAlphaTilt(self.minimum_tilt.val)
         print '-----min tilt-----'
@@ -158,9 +177,4 @@ class EMTomographySeries(Measurement):
     def update_display(self):        
         self.gui.app.processEvents()
 
-class AcquiredSingleImage():
-    def __init__(self,acqData,acqParams,acqTime,staParams):
-        self.acqData = acqData
-        self.acqParams = acqParams
-        self.acqTime = acqTime
-        self.staParams = staParams
+
