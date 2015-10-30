4d47756f3b49243e3bac9ffda8cff60067601ed9
Frank Ogletree <dfogletree@lbl.gov>
2015-10-29 17:30:38 -0700

    sem_slowscan: Slow scan of SEM hardware based on SimpleXYScan example and NI DAC and ADC I/O

---
+++ hardware_components/sem_singlechan_signal.py
@@ -0,0 +1,70 @@
+from . import HardwareComponent
+try:
+    from equipment.NI_Daq import Adc
+except Exception as err:
+    print "Cannot load NI_Daq Adc:", err
+    
+import time
+import random
+
+class SEMSingleChanSignal(HardwareComponent):
+
+    name = "sem_singlechan_signal"
+
+    def setup(self):
+
+        # Create logged quantities
+        self.sem_signal = self.add_logged_quantity(
+                                name = 'sem_signal', 
+                                initial = 0,
+                                dtype=float, fmt="%e", ro=True,
+                                unit="V",
+                                vmin=-100, vmax=100)
+        self.sem_signal_nsamples = self.add_logged_quantity(
+                                name = 'sem_signal_nsamples',
+                                initial=1,
+                                dtype=int, ro=False,
+                                unit = "",
+                                vmin = 1, vmax=10000)
+
+        self.dummy_mode = self.add_logged_quantity(name='dummy_mode', dtype=bool, initial=False, ro=False)
+        
+    def connect(self):
+        if self.debug_mode.val: print "Connecting to NI_Dac Adc"
+        
+        # Open connection to hardware
+
+        if not self.dummy_mode.val:
+            # Normal APD:  "/Dev1/PFI0"
+            # APD on monochromator: "/Dev1/PFI2"
+            self.adc = Adc('X-6368/ai1')
+        else:
+            if self.debug_mode.val: print "Connecting to NI_Dac Adc (Dummy Mode)"
+
+        # connect logged quantities
+        self.sem_signal.hardware_read_func = self.read_signal
+
+
+    def disconnect(self):
+        #disconnect hardware
+        self.adc.close()
+        
+        #disconnect logged quantities from hardware
+        for lq in self.logged_quantities.values():
+            lq.hardware_read_func = None
+            lq.hardware_set_func = None
+        
+        # clean up hardware object
+        del self.adc
+        
+    def read_signal(self):
+        if not self.dummy_mode.val:
+            x = 0
+            for i in range(self.sem_signal_nsamples.val):
+                x += self.adc.get()
+            return x / self.sem_signal_nsamples.val
+        else:
+            time.sleep(self.sem_signal_nsamples.val * 20e-6)
+            signal = random.random()
+            if self.debug_mode.val: print self.name, "dummy read_count_rate", signal
+            return signal
---
+++ hardware_components/sem_slowscan_vout.py
@@ -0,0 +1,63 @@
+'''
+Created on Oct 29, 2015
+
+@author: NIuser
+'''
+from . import HardwareComponent
+from equipment.NI_Daq import Dac
+import random
+
+
+
+class SEMSlowscanVout(HardwareComponent):
+    
+    name = "sem_slowscan_vout"
+    
+    def setup(self):
+        lq_params = dict(  dtype=float, ro=False,
+                           initial = 0,
+                           vmin=-10,
+                           vmax=10,
+                           si = False,
+                           unit='V')
+        self.x_position = self.add_logged_quantity("x_position", **lq_params)
+        self.y_position = self.add_logged_quantity("y_position", **lq_params)       
+
+        self.x_position.reread_from_hardware_after_write = False
+        self.x_position.spinbox_decimals = 3
+        
+        self.y_position.reread_from_hardware_after_write = False
+        self.y_position.spinbox_decimals = 3
+        
+#        self.x_chan = self.add_logged_quantity('x_chan', dtype=str, initial='/X')
+
+    def connect(self):
+        if self.debug_mode.val: print "connecting to NI Dac output task"
+
+        # Open connection to hardware
+        self.dac = Dac('X-6368/ao0:1','SEM_slowscan_dac_out')
+        self.dac.start()
+
+        # connect logged quantities
+        self.x_position.hardware_set_func  = self.write_x
+        self.y_position.hardware_set_func  = self.write_y
+
+    def disconnect(self):
+        if self.debug_mode.val: print "disconnecting from NI Dac output"
+        
+        #disconnect logged quantities from hardware
+        for lq in self.logged_quantities.values():
+            lq.hardware_read_func = None
+            lq.hardware_set_func = None
+        
+        #disconnect hardware
+        self.dac.close()
+        # clean up hardware object
+        del self.dac
+
+
+    def write_x(self,val):
+        self.dac.set((val,self.y_position.val))
+
+    def write_y(self,val):
+        self.dac.set((self.x_position.val,val))
