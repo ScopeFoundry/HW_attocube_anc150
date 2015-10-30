a5b5b6f16dce02dabb726c25c97c15ead6cada23
Zach Anderson <andersonzacharyt@gmail.com>
2015-10-30 16:47:08 -0700

    em_series_run: updated to latest keys

--- em_series_run.py
+++ em_series_run.py
@@ -4,7 +4,7 @@ from PySide import QtGui
 from base_gui import BaseMicroscopeGUI
 
 # Import Hardware Components
-from foundry_scope.hardware_components.tem_hardware import EMHardwareComponent
+from foundry_scope.hardware_components.em_hardware import EMHardwareComponent
 
 # Import Measurement Components
 from measurement_components.em_series import SeriesMeasurement
@@ -22,9 +22,8 @@ class EM_Acquisition(BaseMicroscopeGUI):
         print "Create Measurement objects"
         self.add_measurement_component(SeriesMeasurement(self))
 
-        
         #set some default logged quantities
-        self.hardware_components['em_acquirer'].debug_mode.update_value(True)
+        self.hardware_components['em_hardware'].debug_mode.update_value(True)
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
