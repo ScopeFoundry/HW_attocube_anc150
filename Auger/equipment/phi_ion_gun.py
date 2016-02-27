bc3eda308c6d42528fcb7db8f58f147f38575f9f
Alan Buckley <alanbuckley@lbl.gov>
2016-02-26 16:56:12 -0800

    equipment/phi_ion_gun: handle multi-threaded access to hardware (ask_in_progress flag). Additional debug strings added

--- Auger/equipment/phi_ion_gun.py
+++ Auger/equipment/phi_ion_gun.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import chunk
 
 
 class PhiIonGun(object):
@@ -12,8 +13,10 @@ class PhiIonGun(object):
         if self.debug: print "Phi Ion Gun init, port=%s" % self.port
         
         self.ser = serial.Serial(port=self.port, baudrate=9600, timeout=1.0)
-        
+        self.ask_in_progress = False        
         self.initialize()
+        
+
         
    
     def ask_cmd(self, cmd_id, data=None):
@@ -23,23 +26,37 @@ class PhiIonGun(object):
         then hex checksum and 'CR' 
         All commands return status ('address' OK 00 ) and optional data plus checksum
         '''
-        self.cmd_id = cmd_id
-        msg = "~ %02X %02X " % (self.address, self.cmd_id)
+        
+        for i in range(10):
+            if self.ask_in_progress:
+                time.sleep(0.1)
+            else:
+                break
+        if self.ask_in_progress:
+            raise IOError("phi_ion_gun could not ask_cmd, ask_in_progres")
+        
+        try:
+            self.ask_in_progress = True
 
-        if data:
-            msg += data + " "
+            self.cmd_id = cmd_id
+            msg = "~ %02X %02X " % (self.address, self.cmd_id)
     
-        checksum = self.checksum_func(msg[1:])
-        full_msg = msg + ("%02X\r" % checksum)
+            if data:
+                msg += str(data) + " "
         
-        if self.debug:
-            print "ask_cmd write->", repr(full_msg)
-    
-        self.ser.write(full_msg)
-        resp = self.my_readline()      
-        if self.debug:
-            print "ask_cmd resp->", repr(resp)
+            checksum = self.checksum_func(msg[1:])
+            full_msg = msg + ("%02X\r" % checksum)
+            
+            if self.debug:
+                print "ask_cmd write->", repr(full_msg)
         
+            self.ser.write(full_msg)
+            resp = self.my_readline()      
+            if self.debug:
+                print "ask_cmd resp->", repr(resp)
+
+        finally:
+            self.ask_in_progress = False
         
         #assert resp[0:8] == "%02X OK 00 " % self.address
         #assert self.checksum_func(resp[:-3]) == int(resp[-3:-1],16)
@@ -60,8 +77,11 @@ class PhiIonGun(object):
         return line
         
     def parse_data(self, response):
-        return response.split()[3]
-        
+        chunk = response.split()[3]
+        return chunk
+        if self.debug:
+            print "Chunk:", chunk
+             
     def checksum_func(self, data):
         checksum = 0x00
         for b in data:
@@ -133,14 +153,23 @@ class PhiIonGun(object):
     def initialize(self):
         if self.debug: 
             print "initialize"
-        resp = self.ask_cmd(0x05)
-        return resp
+        self.ask_cmd(0x01)
+        self.ask_cmd(0x02)
+        self.ask_cmd(0x05)
+        self.ask_cmd(0x33)
+        self.ask_cmd(0x1E, 1)
+        self.ask_cmd(0x1F)
+        self.ask_cmd(0x32, 3)
+        self.ask_cmd(0x39)
+        self.ask_cmd(0x3C)
+        self.ask_cmd(0x39)
+        self.State_Data_Packet()
+        
 
     def read_condenser_v(self):
         resp = self.ask_cmd(0x10)
         value = self.parse_data(resp)
-        if self.debug:
-            print "condenser_v: ",value 
+        if self.debug:  print "read_condenser_v: ",value 
         return float(value)
     
     def read_emission_current(self): 
@@ -152,7 +181,7 @@ class PhiIonGun(object):
     def read_energy(self):
         resp = self.ask_cmd(0x0C) 
         value = self.parse_data(resp)
-        if self.debug: print "energy:", resp
+        if self.debug: print "read_energy:", resp
         return float(value)
     
     def read_extractor_p(self):
@@ -164,7 +193,7 @@ class PhiIonGun(object):
     def read_float(self):
         resp = self.ask_cmd(0x12)
         value = self.parse_data(resp)
-        if self.debug: print "float_v", resp
+        if self.debug: print "float_v:", resp
         return float(value)
     
     def read_objective_v(self):
@@ -176,82 +205,95 @@ class PhiIonGun(object):
     def write_energy(self, data):
         _float = ("%.3f" % data)
         _energy = self.ask_cmd(0x0A, _float)
+        if self.debug: print "write_energy"
         return _energy
     
     def write_beam_v(self, data):
         _float = ("%.3f" % data)
         beam_v = self.ask_cmd(0x0B, _float)
+        if self.debug:
+            print "write_beam_v"
         return beam_v 
 
     def write_grid_v(self, grid_v):
         assert 99 < grid_v < 201
         _float = ("%.3f" % grid_v)
         grid_v = self.ask_cmd(0x13, _float)
+        if self.debug: print "write grid_v"
         return grid_v
 
     def write_condenser_v(self, data):
         _float = ("%.3f" % data)
         cond_v = self.ask_cmd(0x0F, _float)
+        if self.debug: print "write_condenser_v"
         return cond_v
 
     def write_objective_v(self, data):
         _float = ("%.3f" % data)
         obj_v = self.ask_cmd(0x15, _float)
+        if self.debug: print "write_objective_v"
         return obj_v
 
     def write_bend_v(self, data):
         _float = ("%.3f" % float(-1*data))
         bend_v = self.ask_cmd(0x0D, _float)
+        if self.debug: print "write_bend_v"
         return bend_v
     
     def write_emission_current(self, data):
         #Note: This is the same function listed as "Current" in other program
         _float = ("%.3f" % data)
         emi_i = self.ask_cmd(0x17, _float)
+        if self.debug: print "write_emission_current"
         return emi_i
 
     def write_float_v(self, data):
         _float = ("%.3f" % float(-1*data))
         float_v = self.ask_cmd(0x11, _float)
+        if self.debug: print "write_float_v"
         return float_v
 
     def xsize(self, data):
         _float = ("%.2f" % data)
         xmm = self.ask_cmd(0x43, _float)
+        if self.debug: print "set_xsize"
         return xmm
     
     def ysize(self, data):
         _float = ("%.2f" % data)
         ymm = self.ask_cmd(0x44, _float)
+        if self.debug: print "set_ysize"
         return ymm
     
     def xoff(self, data):
         _float = ("%.3f" % data)
         _xoff = self.ask_cmd(0x45, _float)
+        if self.debug: print "set_xoff"
         return _xoff
 
     def yoff(self, data):
         _float = ("%.3f" % data)
         _yoff = self.ask_cmd(0x46, _float)
+        if self.debug: print "set_yoff"
         return _yoff
 
 ##---------- Commonly found entities----------##
     def Header(self):
         _header = self.ask_cmd(0x39)
         if self.debug:
-            print _header
+            print "Header:", _header
         return _header
         
     def Footer(self):
-        _header = self.ask_cmd(0x48)
+        _footer = self.ask_cmd(0x48)
         if self.debug:
-            print _header
-        return _header
+            print "Footer:", _footer
+        return _footer
 
     def Write_Confirmation(self):
         statement = self.ask_cmd(0x28)
         if self.debug:
-            print statement
+            print "Write confirm:", statement
         return statement
 
     def Gun_Inactive(self):
@@ -263,7 +305,7 @@ class PhiIonGun(object):
     def Gun_Active(self):
         Gun_on = self.ask_cmd(0x35)
         if self.debug:
-            print "Gun active: ", Gun_on
+            print "Gun Active: ", Gun_on
         return Gun_on
 
     def Set_Raster_Mode(self, State='OFF'):
@@ -305,7 +347,7 @@ class PhiIonGun(object):
                 print "External Raster Set!"
 
 ##--edited below--##
-    def State_Data_Packet(self, beamv=0,gridv=0, condv=0, objv=0, bendv=0, emiv=0, State='OFF'):
+    def State_Data_Packet(self, beamv=0, gridv=0, condv=0, objv=0, bendv=0, emiv=0, State='OFF'):
         assert State in ['OFF', 'BLANK', 'STANDBY', 'ACTIVE']
         if State == 'STANDBY':
             bendv = -7.0 
@@ -332,7 +374,8 @@ class PhiIonGun(object):
         state_data = "1 {beamv:.3f} {gridv:.3f} {condv:.3f} {objv:.3f}".format(beamv=beamv, gridv=gridv, condv=condv, objv=objv)
         state_data += " {bendv:.3f} 0.000 {emiv:.3f} 51 1".format(bendv=bendv, emiv=emiv)
         state_data += " {:.3f} {:.3f} {:.3f} {:.3f}".format(*States)
-        print(">>", state_data)
+        if self.debug:
+            print(">>", state_data)
         #Note: The above function is not used to set the float voltage. Use specific command to write your float voltage.
         set_state = self.ask_cmd(0x1C, state_data)
         return set_state
@@ -416,15 +459,15 @@ if __name__ == '__main__':
     phi.Startup_Commands()
 
     #phi.read_condenser_v()
-    print "Emission {:.1f} mA".format(phi.read_current())
-    print "Beam {:.1f} V".format(phi.read_energy())
-    print "Condenser {:.1f} V".format(phi.read_condenser_v())
-    print "Objective {:.1f} V".format(phi.read_objective_v())
-    print "Float {:.1f} V".format(phi.read_float())
-    print phi.read_version()
-    for i in range(10):
-        time.sleep(0.1)
-        print "Emission {:.2f} mA".format(phi.read_current())
-    time.sleep(1)
+    #print "Emission {:.1f} mA".format(phi.read_current())
+    #print "Beam {:.1f} V".format(phi.read_energy())
+    #print "Condenser {:.1f} V".format(phi.read_condenser_v())
+    #print "Objective {:.1f} V".format(phi.read_objective_v())
+    #print "Float {:.1f} V".format(phi.read_float())
+    #print phi.read_version()
+    #for i in range(10):
+    #    time.sleep(0.1)
+    #    print "Emission {:.2f} mA".format(phi.read_current())
+    #time.sleep(1)
     phi.close()
     
