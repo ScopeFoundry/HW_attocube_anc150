521ba9c1225218029ddac588ce6a70dbbf994264
Alan Buckley <alanbuckley@lbl.gov>
2016-06-14 15:06:58 -0700

    Believed to be the last tested and working files from before our major NanoAuger equipment crash.

---
+++ NIFPGA/Alpha.py
@@ -0,0 +1,204 @@
+import ctypes
+#import time
+from ctypes import windll
+import csv
+import numpy as np
+from collections import OrderedDict
+
+
+fpga_dll = windll.LoadLibrary('NIFpga.dll')
+#test_array = np.array([1,0,1,1,1,0,0,0])
+#test_array2 = np.array([1,0,0,0,0,0,0,1])
+#size_4  = np.array([1,0,1,0])
+#bitfilename = r"C:\Users\NIuser\Documents\Programs LV\R Series\builds\Omicron_R_1\Omicron Auger\data\NiFpga_CountertoDAC.lvbitx"
+#signature = "146CFF8F04265BED0C2F87F8C0A0672A"
+#resource = "RIO0"
+###--------------End Basic Command Section----------------###
+
+class NI_FPGA(object):        
+    def __init__(self, bitfilename, signature, resource="RIO0"):
+        self.session = ctypes.c_uint32(0)
+        self.bitfilename = bitfilename
+        self.signature = signature
+        self.resource = resource
+        self.fifo = ctypes.c_uint32(0)
+        # NOTE: CounterFifo declared in last lines of
+        #NiFpga_CountertoDAC.h as I32 datatype with value of 0
+        #However, self.fifo argument warrants use of U32 type   (:c) <-- Disappointment.
+        self.test_array = np.array([1,0,1,1,1,0,0,0])
+        self.test_array2 = np.array([1,0,0,0,0,0,0,1])
+        # Load error code information from csv file derived from header file
+        self.err_code_list = []
+        with open('nifpga_errorcodes_14.csv') as err_file:
+            csv_read = csv.reader(err_file, quotechar='"') #np.array([])
+            for row in csv_read:
+                self.err_code_list.append(row)
+                # np.concatenate((array1, row), axis=0)
+            #result = np.reshape(array1,(49,3))
+            #print(self.err_code_list)
+        self.err_code_list = self.err_code_list[1:]
+        self.err_bynum = OrderedDict()
+        for code, name, description in self.err_code_list:
+            num = int(code.replace('_','-'))
+            self.err_bynum[num] = name + ": " + description 
+            
+            
+    def handle_err(self, err_code):
+        if err_code != 0:
+            #print "Error {}. Consult FPGA Interface C API > API Reference > Errors using provided status code for more details ".format(err_code)
+            #print "Error {}: {}".format(err_code, self.err_bynum[err_code])
+            raise IOError("NI_FPGA Error {}: {}".format(err_code, self.err_bynum[err_code]))
+        return err_code
+    
+    def connect(self):
+        NiFpga_OpenAttribute_NoRun = 1 # from Nifpga.h
+        attribute = NiFpga_OpenAttribute_NoRun
+        session=self.session
+        err = self.handle_err(fpga_dll.NiFpgaDll_Open(self.bitfilename, self.signature, self.resource, attribute, ctypes.byref(session)))
+        print "Connection Status:" + str(err), self.session
+        if err == 0:
+            print "Connected, FPGA configured to automatically run."
+            
+    def unload(self):
+        err = self.handle_err(fpga_dll.NiFpgaDll_Finalize())
+        print "Unload Status:" + str(err), self.session
+        if err == 0:
+            print "FPGA Library unloaded (caveat: not thread safe)."
+            
+    def disconnect(self):
+        err = self.handle_err(fpga_dll.NiFpgaDll_Close(self.session, 0))
+        print "Disconnect Status:" + str(err)
+        if err == 0:
+            print "FPGA session disconnected (closed)."
+        
+    def run(self):
+        self.attribute = 0
+        err = self.handle_err(fpga_dll.NiFpgaDll_Run(self.session, self.attribute))
+        print "Status:" + str(err)
+        if err == 0:
+            print "Running FPGA VI on target."
+    #Runs the FPGA VI on the target. If you use 
+    #NiFpga_RunAttribute_WaitUntilDone, 
+    #NiFpga_Run blocks the thread until the FPGA finishes running.
+    
+    def abort(self):
+        err = self.handle_err(fpga_dll.NiFpgaDll_Abort(self.session))
+        print "Status:" + str(err)
+        if err == 0:
+            print "FPGA VI successfully aborted."
+    #Aborts the FPGA VI.
+    
+    
+    def reset(self):
+        err = fpga_dll.NiFpgaDll_Reset(self.session)
+        print "Status:" + str(err)
+        if err == 0:
+            print "FPGA VI successfully reset."
+    #Resets the FPGA VI.
+    
+    
+    def download(self):
+            err = self.handle_err(fpga_dll.NiFpgaDll_Download(self.session))
+            print "Status:" + str(err)
+            if err == 0:
+                print "FPGA VI successfully downloaded."
+    #Redownloads FPGA to target.
+
+###--------------End Basic Command Section----------------###
+
+##### -----------Begin FIFO Functions ---------######
+
+#NiFpgaDll_ConfigureFifo2
+#NiFpgaDll_StopFifo
+#NiFpgaDll_StartFifo
+
+# NOTE: CounterFifo declared in last lines of
+#       NiFpga_CountertoDAC.h as I32 datatype with value of 0
+
+    def Configure_Fifo2(self, reqDepth=8000):
+        self.requested_depth = ctypes.c_size_t(reqDepth)
+        self.actual_depth = ctypes.c_size_t(0)
+        err = fpga_dll.NiFpgaDll_ConfigureFifo2(self.session, self.fifo, self.requested_depth, self.actual_depth)
+        print "FIFO Configure Status:" + str(err)
+        if err == 0:
+            print "FIFO Configured"
+    
+    ##Optional Method:  
+    def Start_Fifo(self):
+        err = fpga_dll.NiFpgaDll_StartFifo(self.session, self.fifo)
+        print "Start FIFO Status:" + str(err)
+        if err == 0:
+            print "FIFO Started"
+
+    def Read_Fifo(self, timeout=5000):
+        self.data = ctypes.c_uint32(0)
+        self.size = ctypes.c_size_t(8)
+        self.timeout = ctypes.c_uint32(timeout)
+        self.remaining = ctypes.c_size_t(0)
+        err = fpga_dll.NiFpgaDll_ReadFifoU32(self.session, self.fifo, self.data, self.size, self.timeout, self.remaining)
+        print "Read FIFO Status:" + str(err)
+        if err == 0:
+            print "FPGA FIFO Read."
+        #Returns Data as "Last" I32
+
+    ##Optional Method:
+    def Stop_Fifo(self):
+        err = fpga_dll.NiFpgaDll_StopFifo(self.session, self.fifo)
+        print "Stop FIFO Status:" + str(err)
+        if err == 0:
+            print "FPGA VI successfully reset."
+
+##### -----------End FIFO Functions ---------######
+### -----------Begin Read/Write Functions ------###
+
+    def Read_Bool(self, indicator):
+        data = ctypes.c_char(0)
+        err = fpga_dll.NiFpgaDll_ReadBool(self.session, indicator, ctypes.byref(data))
+        return err, data.value
+
+    def Read_I16(self, indicator):
+        data = ctypes.c_int16(0)
+        err = fpga_dll.NiFpgaDll_ReadI16(self.session, indicator, ctypes.byref(data))
+        return err, data.value
+
+    def Read_U16(self, indicator):
+        data = ctypes.c_uint32(0)
+        err = fpga_dll.NiFpgaDll_ReadU16(self.session, indicator, ctypes.byref(data))
+        return err, data.value
+
+    def Read_U32(self, indicator):
+        data = ctypes.c_uint32(0)
+        err = fpga_dll.NiFpgaDll_ReadU32(self.session, indicator, ctypes.byref(data))
+        return err, data.value
+    
+
+    def Read_ArrayBool(self, indicator, size):
+        self.data = np.zeros(shape=size, dtype=np.byte)
+        pointer = self.data.ctypes.data_as(ctypes.POINTER(ctypes.c_char)) #  <-- Not sure if variable name is necessary
+        err = fpga_dll.NiFpgaDll_ReadArrayBool(self.session, indicator, pointer, size)
+        return err, self.data ## ** data.value could quite possibly be problematic due to structural reasons
+
+    def Read_ArrayU32(self, indicator, size):
+        self.data = np.zeros(shape=size, dtype=np.uint32)
+        pointer = self.data.ctypes.data_as(ctypes.POINTER(ctypes.c_uint32)) #  <-- Not sure if variable name is necessary
+        err = fpga_dll.NiFpgaDll_ReadArrayU32(self.session, indicator, pointer, size)
+        return err, self.data
+
+    
+    def Write_ArrayBool(self, indicator, array, size):
+        self.data = array #<-- We are writing array. This won't be overwritten. Needs to receive something to write 
+        pointer = self.data.ctypes.data_as(ctypes.POINTER(ctypes.c_char))
+        err = fpga_dll.NiFpgaDll_WriteArrayBool(self.session, indicator, pointer, size)
+        return err
+### -----------End Read/Write Functions ------###
+
+
+
+###--------------Begin Executable Section-----------------###
+
+#if __name__ == '__main__':
+    
+    #array_gen()
+    #print fpga.err_code_list
+
+
---
+++ NIFPGA/Counter_DAC_VI.py
@@ -0,0 +1,209 @@
+## Labview VI Functions 
+from Alpha import NI_FPGA
+import ctypes
+
+
+class Counter_DAC(NI_FPGA):        
+    
+    bitfilename = r"C:\Users\NIuser\Documents\Programs LV\R Series\builds\Omicron_R_1\Omicron Auger\data\NiFpga_CountertoDAC.lvbitx"
+    signature = "146CFF8F04265BED0C2F87F8C0A0672A"
+    resource = "RIO0"
+    session = ctypes.c_uint32(0)
+    
+    def __init__(self):
+        NI_FPGA.__init__(self, Counter_DAC.bitfilename, Counter_DAC.signature, Counter_DAC.resource)
+
+    FPGA = NI_FPGA(bitfilename, signature, resource) 
+    #Scoping issue solved by taking this definition (FPGA shown above) out of init header. Yuss! :poop: :D 11/22/15
+    
+    def CtrElapsed(self):
+        self.indicator = 0x8124
+        err, value = self.FPGA.Read_U32(self.indicator)
+        if err == 0:
+            print "CtrElapsed Read:", value
+        else:
+            print "Status:" + str(err)
+
+    def CtrTransfer(self):
+        self.indicator = 0x8128
+        err, value = self.FPGA.Read_U32(self.indicator)
+        if err == 0:
+            print "CtrTransfer Read:", value
+        else:
+            print "Status:" + str(err)
+
+    def CtrOverflow(self):
+        self.indicator = 0x812E
+        err = self.FPGA.Read_Bool(self.indicator)
+        print "Status:" + str(err)
+        if err == 0:
+            print "CtrOverflow Read"
+    
+
+    def Read_DAC1(self):
+        self.indicator = 0x814A
+        err, value = self.FPGA.Read_I16(self.indicator)     
+        if err == 0:
+            print "DAC1 Read:", value
+        else:
+            print "DAC1 Error:" + str(err)
+            
+
+        
+    def Read_DAC2(self):
+        self.indicator = 0x8146
+        err, value = self.FPGA.Read_I16(self.indicator)
+        if err == 0:
+            print "DAC2 Read:", value
+        else:
+            print "DAC2 Error:" + str(err)
+
+            #Current error 63195, invalid session.
+
+    def Loop_Elapsed(self):
+        self.indicator = 0x8142
+        err, value = self.FPGA.Read_U16(self.indicator)
+        if err == 0:
+            print "Loop Elapsed:", value
+        else:
+            print "Loop Elapsed Error:" + str(err)
+
+
+    def CtrFIFO(self, _bool="0"):
+        self.control= ctypes.c_uint32(0x8132)
+        self.value = ctypes.c_char(_bool)
+        #err, value = self.FPGA.Write             ###Update definitions in subordinate NI_FPGA python file###   
+        #err = self.handle_err(fpga_dll.NiFpgaDll_WriteBool(self.session, self.control, self.value))
+        print "Status:" + str(err)
+        if err == 0:
+            print "CtrFIFO Toggled"
+
+
+    def Offset1(self, _Offset=0):
+        self.control= ctypes.c_uint32(0x811E)
+        self.value = ctypes.c_int16(_Offset)
+        err = self.handle_err(fpga_dll.NiFpgaDll_WriteI16(self.session, self.control, self.value))
+        print "Status:" + str(err)
+        if err == 0:
+            print "Offset1 Set"
+
+    def Offset2(self, _Offset=0):
+        self.control= ctypes.c_uint32(0x811A)
+        self.value = ctypes.c_int16(_Offset)
+        err = self.handle_err(fpga_dll.NiFpgaDll_WriteI16(self.session, self.control, self.value))
+        print "Status:" + str(err)
+        if err == 0:
+            print "Offset2 Set"
+
+    def Scale1(self, _scale=0):
+        self.control= ctypes.c_uint32(0x8116)
+        self.value = ctypes.c_int16(_scale)
+        err = self.handle_err(fpga_dll.NiFpgaDll_WriteI16(self.session, self.control, self.value))
+        print "Status:" + str(err)
+        if err == 0:
+            print "Scale1 Set"
+
+    def Scale2(self, _scale=0):
+        self.control= ctypes.c_uint32(0x8112)
+        self.value = ctypes.c_int16(_scale)
+        err = self.handle_err(fpga_dll.NiFpgaDll_WriteI16(self.session, self.control, self.value))
+        print "Status:" + str(err)
+        if err == 0:
+            print "Scale1 Set"
+
+    def Counter_ticks(self, _counterticks=40000):
+        self.control= ctypes.c_uint32(0x8120)
+        self.value = ctypes.c_uint32(_counterticks)
+        err = self.handle_err(fpga_dll.NiFpgaDll_WriteU32(self.session, self.control, self.value))
+        print "Status:" + str(err)
+        if err == 0:
+            print "Counter Ticks Set"
+
+
+    def Rate(self, _rate=400):
+        self.control= ctypes.c_uint32(0x810C)
+        self.value = ctypes.c_uint32(_rate)
+        err = self.handle_err(fpga_dll.NiFpgaDll_WriteU32(self.session, self.control, self.value))
+        print "Status:" + str(err)
+        if err == 0:
+            print "Rate Set"
+
+
+    def DIO811_Read(self):
+        self.size = 4
+        err, array = self.Read_ArrayBool(0x813A, self.size)
+        if err == 0:
+            print "DIO811 Read:", array
+        else:
+            print "DIO811 Status:" + str(err)
+
+## Lists cumulative counter hit number as a row of 8 integer vales, 
+### each entry represents the counter value on each of 8 channels.
+    def Counts(self):
+        self.size = 8
+        err, array = self.Read_ArrayU32(0x813C, self.size)
+        if err == 0:
+            print "Counts Read:", array
+        else:
+            print "Counts Status:" + str(err)
+
+
+## The following functions allow you to write a single row numpy array of boolean values to the counter to dac fpga vi
+## If no errors arise, the function prints the input array for your verification.
+    def DAC1_add(self, bool_array):
+        self.array = bool_array
+        self.indicator= ctypes.c_uint32(0x815A)
+        self.size = 8
+        err = self.Write_ArrayBool(self.indicator, self.array, self.size)
+        print "Status:" + str(err)
+        if err == 0:
+            print "\"DAC1 add\" array successfully written:", self.array
+
+    def DAC1_sub(self, bool_array):
+        self.array = bool_array
+        self.indicator= ctypes.c_uint32(0x8156)
+        self.size = 8
+        err = self.Write_ArrayBool(self.indicator, self.array, self.size)
+        print "Status:" + str(err)
+        if err == 0:
+            print "\"DAC1 sub\" array successfully written:", self.array
+            
+    def DAC2_add(self, bool_array):
+        self.array = bool_array
+        self.indicator= ctypes.c_uint32(0x8152)
+        self.size = 8
+        err = self.Write_ArrayBool(self.indicator, self.array, self.size)
+        print "Status:" + str(err)
+        if err == 0:
+            print "\"DAC2 add\" array successfully written:", self.array
+
+    def DAC2_sub(self, bool_array):
+        self.array = bool_array
+        self.indicator= ctypes.c_uint32(0x814E)
+        self.size = 8
+        err = self.Write_ArrayBool(self.indicator, self.array, self.size)
+        print "Status:" + str(err)
+        if err == 0:
+            print "\"DAC2 sub\" array successfully written:", self.array
+
+    def DIO1215_Write(self, bool_array):
+        self.array = bool_array
+        self.indicator= ctypes.c_uint32(0x8136)
+        self.size = 4
+        err = self.Write_ArrayBool(self.indicator, self.array, self.size)
+        print "Status:" + str(err)
+        if err == 0:
+            print "\"DIO1215 Write\" array successfully written:", self.array
+###--------------End Counter to DAC controls and indicators------------------###
+
+
+if __name__ == '__main__':
+    vi = Counter_DAC()
+    fpga = Counter_DAC.FPGA
+    fpga.connect()
+    fpga.reset()
+    fpga.run()
+    vi.CtrElapsed()
+    vi.Read_DAC1()
+    vi.Read_DAC2()
+    fpga.disconnect()
