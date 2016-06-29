ad015b5c238379708776b5b3cb62ba59759ca131
Edward Barnard <esbarnard@lbl.gov>
2016-06-29 09:31:34 -0700

    auger_electron_analyzer: working version with ScopeFoundry interface and prologix GPIB connection

--- Auger/auger_electron_analyzer.py
+++ Auger/auger_electron_analyzer.py
@@ -10,23 +10,29 @@ class AugerElectronAnalyzer(object):
     "Omicron Multiplier Supplies 1127"
     "Omicron Mulit-channel receiver 538"
     
-    def __init__(self, debug=True):
+    def __init__(self, debug=False):
         
         self.debug=debug
         
-        self.work_function = 4.5
         
         # prologix usb-gpib
-        self.ser = Serial("COM8", )
+        self.gpib = PrologixGPIB_Omicron("COM8", debug=True)
+
+        # Initialize to known state
+        self.retarding_mode='CAE'
+        self.KE_V = 0.0
+        self.set_work_function(4.5)
+        self.write_multiplier_state(False)
+        self.write_crr_ratio(1.5)
+        self.write_pass_energy(5.0)
+        self.write_KE(0.0)
+        for quad in ['X1', 'Y1', 'X2', 'Y2']:
+            self.write_Quad(quad, 0.0)
         
-    def write_ser(self, x):
-        print( "write_ser", repr(x))
-        self.ser.write(x)
         
-    def write_cmd(self, address, cmd_num, value):
-        print("address", address)
-        print("cmd", hex(cmd_num))
-        print('value', hex(value))
+        
+    def omicron_cmd_encode(self, cmd, val ):
+        '''cmd is command number, val is uint 16, format for omicron '''
         # GPIB commands of the form:
         # 0x83|00|1A
         # 3 bytes:
@@ -38,38 +44,25 @@ class AugerElectronAnalyzer(object):
         # 0000 0000 | p000 0000 | q000 0000
         # to
         # 0pq0 0000 | 0000 0000 | 0000 0000
-                
-                
+
+        cmd &= 0x8f #commands are 0x8#
+        v1 = (val & 0xff00)>>8
+        v2 = val & 0xff
+        if v1 & 0x80:
+            cmd |= 0x40
+            v1  &= 0x7f
+        if v2 & 0x80:
+            cmd |= 0x20
+            v2  &= 0x7f
+        s = b'' + chr(cmd) + chr(v1) + chr(v2)
+        return s    
     
-        cmd_bytes = (
-            (cmd_num<<16) + value)
-            # move 0x80 bit to 0x200000
-            #((value & (1<<7 )) << (22-8 )) - (value & (1<<7 )) + 
-            # move 0x8000 bit to 0x400000
-            #((value & (1<<15)) << (23-16)) - (value & (1<<15))
-            #((value & (1<<7 )) << (22-8 ))  +  ((value & (1<<15)) << (23-16)) + (value ^ ((1<<15) + (1<<7 )))
-            #)
-        
-        
-        print(hex(cmd_bytes))
-        
-        _bytes = [(cmd_bytes >> i & 0xff) for i in (16,8,0)]   
-        
+    def write_cmd(self, address, cmd_num, value):
+        '''cmd is command number, val is uint 16'''
         # prologix usb-gpib
-        self.write_ser("++addr%i\n" % address)
-        esc_bytes = []
-        for byte in _bytes:
-            if byte in (13,10,27,43):
-                esc_bytes.append(27)
-            esc_bytes.append(byte)
-        print("cmd_bytes" , (hex(cmd_bytes)))
-        print("bytes", repr(bytes))
-        print("esc", repr(esc_bytes))
-        print(bytes(esc_bytes))
-        self.write_ser(bytes(esc_bytes))
-        
-        #self.dev[address].write(cmd_bytes)
-        
+        self.gpib.set_address(address)
+        s = self.omicron_cmd_encode(cmd_num, value)
+        self.gpib.write(s)
         
     def get_work_function(self):
         return self.work_function
@@ -94,8 +87,15 @@ class AugerElectronAnalyzer(object):
         self.multiplier_state = bool(multiplier_state)
         assert retarding_mode in self.retarding_modes.keys()
         self.retarding_mode = retarding_mode
-        val = 0x04*bool(multiplier_state) + 0x02*self.retarding_modes[retarding_mode]
+        val = 0x0400*bool(multiplier_state) + 0x0200*self.retarding_modes[retarding_mode]
         self.write_cmd(1, 0x85, val)
+        
+        # Alternate method
+        # state is a special command that can be only 2 bytes long (instead of 3)
+        #val = 0x04*bool(multiplier_state) + 0x02*self.retarding_modes[retarding_mode]
+        #self.gpib.set_address(1)
+        #self.gpib.write(b"\x85" + chr(val))
+
 
     def write_multiplier_state(self, state):
         return self.write_state(state, self.retarding_mode)
@@ -104,7 +104,7 @@ class AugerElectronAnalyzer(object):
         return self.multiplier_state
 
     def write_retarding_mode(self, retarding_mode):
-        return self.write_mode(self.multiplier_state, retarding_mode)
+        return self.write_state(self.multiplier_state, retarding_mode)
     
     def get_retarding_mode(self):
         return self.retarding_mode
@@ -124,7 +124,7 @@ class AugerElectronAnalyzer(object):
     # CRR
     def write_crr_ratio(self, crr_ratio):
         assert 1.5 <= crr_ratio <= 20.0
-        self.crr_ratio = self.crr_ratio
+        self.crr_ratio = crr_ratio
         val = int(crr_ratio*3276.8)
         self.resolution_eV = self.KE_V/self.crr_ratio * 0.02
         self.write_cmd(1, 0x84, val)
@@ -144,8 +144,13 @@ class AugerElectronAnalyzer(object):
         assert -0.5 <= val <= 0.5
         val_int = int((val + 0.5)*65535)
         self.write_cmd(3,self.quad_cmds[quad], val_int)
+        
+    def write_Quad_X1(self,val):
+        return self.write_Quad('X1', val)
     
-    
+    def close(self):
+        self.gpib.close()
+        
     # setup procedure
     """
     write multipliers and mode
@@ -165,11 +170,15 @@ class AugerElectronAnalyzerHC(HardwareComponent):
     def setup(self):
         self.settings.New("mode", dtype=str, choices=('CAE', 'CRR'))
         self.settings.New("multiplier", dtype=bool)
-        self.settings.New("KE", dtype=float, unit='V', min=0, max=2200)
-        self.settings.New("work_function", dtype=float, unit='eV', min=0, max=10, initial=4.5)
-        self.settings.New("pass_energy", dtype=float, unit='V', min=5, max=500)
-        self.settings.New("crr_ratio", dtype=float, min=1.5, max=20)
+        self.settings.New("KE", dtype=float, unit='V', vmin=0, vmax=2200)
+        self.settings.New("work_function", dtype=float, unit='eV', vmin=0, vmax=10, initial=4.5)
+        self.settings.New("pass_energy", dtype=float, unit='V', vmin=5, vmax=500)
+        self.settings.New("crr_ratio", dtype=float, vmin=1.5, vmax=20)
         self.settings.New("resolution", dtype=float, ro=True, unit='eV')
+        self.settings.New("quad_X1", dtype=float, vmin=-0.5, vmax=+0.5, initial=0, si=False)
+        self.settings.New("quad_Y1", dtype=float, vmin=-0.5, vmax=+0.5, initial=0, si=False)
+        self.settings.New("quad_X2", dtype=float, vmin=-0.5, vmax=+0.5, initial=0, si=False)
+        self.settings.New("quad_Y2", dtype=float, vmin=-0.5, vmax=+0.5, initial=0, si=False)
     
     def connect(self):
         E = self.e_analyzer = AugerElectronAnalyzer(debug=self.debug_mode.val)
@@ -194,6 +203,14 @@ class AugerElectronAnalyzerHC(HardwareComponent):
 
         self.settings.resolution.hardware_read_func = E.get_resolution
         
+        print("setting quad hardware_set_funcs")
+        self.settings.quad_X1.hardware_set_func = lambda val, E=E: E.write_Quad('X1', val) 
+        self.settings.quad_Y1.hardware_set_func = lambda val, E=E: E.write_Quad('Y1', val) 
+        self.settings.quad_X2.hardware_set_func = lambda val, E=E: E.write_Quad('X2', val) 
+        self.settings.quad_Y2.hardware_set_func = lambda val, E=E: E.write_Quad('Y2', val) 
+        print("done quad hardware_set_funcs")
+        
+        
         for lqname in ['KE', 'pass_energy', 'crr_ratio']:
             getattr(self.settings, lqname).updated_value[None].connect(self.settings.resolution.read_from_hardware)
     
@@ -206,108 +223,131 @@ class AugerElectronAnalyzerHC(HardwareComponent):
         del self.e_analyzer
         
 
-"""from ScopeFoundry import Measurement
 
-class AugerElectronAnalyzerViewer(Measurement):"""
-
-def set_address_gpib(port, address=1):
-    port.write('++addr {:d}\n'.format(address))
-
-def set_config_gpib(port):
-    '''configure prologix usb GPIB for Omicron'''
-    #no automatic read after write
-    port.write("++auto 0\n")
-    #assert gpib EOI after write
-    port.write("++eoi 1\n")
-    #no CR, LF after write
-    port.write("++eos 3\n")
-    #be controller, send to omicron
-    port.write("++mode 1\n")
-    #no CR, LF with read
-    port.write("++eos 3\n")
-
-def get_config_gpib(port):
-    ''' get prologix gpib configuration'''
-    port.write('++ver\n')
-    print( port.readline())
-    port.write("++auto\n")
-    print( 'auto '+port.readline())
-    port.write("++eoi\n")
-    print( 'eoi '+port.readline())
-    port.write("++eos\n")
-    print( 'eos '+port.readline())
-    port.write("++mode\n")
-    print( 'mode '+port.readline())
-    port.write("++addr\n")
-    print( 'address '+port.readline())
+class PrologixGPIB_Omicron(object):
     
-def set_mode_omicron( mult=True, crr=True ):
-    cmd = '\x85'
-    val = 0
-    if mult: val |= 0b100
-    if crr: val |= 0b10
-    s = b'' + cmd + chr(val)
-    return s
+    def __init__(self, port, address=1, debug=False):
+        self.port = port
+        self.ser = Serial(port)
+        self.debug = debug
+        self.write_config_gpib()
+        self.set_address(address)
+        if self.debug:
+            self.read_print_config_gpib()
+        
+    def close(self):
+        return self.ser.close()
 
-def debug_omicron(s):
-    print( " ".join("{:02x}".format(ord(c)) for c in s))
-    print( " ".join("{:08b}".format(ord(c)) for c in s))
+    def set_address(self, address=1):
+        cmd_str = '++addr {:d}\n'.format(address)
+        if self.debug: print("prologix set_addr", repr(cmd_str))
+        self.ser.write(cmd_str)
 
-def gpib_string( s ):
-    ''' prevent binary data from being interpreted
-    as prologix configuration commands, add lf'''
-    esc = chr(27)
-    lf = '\n'
-    cr = chr(0xd)
-    plus = '+'
+    def write_config_gpib(self):
+        '''configure prologix usb GPIB for Omicron'''
+        ser = self.ser
+        #no automatic read after write
+        ser.write("++auto 0\n")
+        #assert gpib EOI after write
+        ser.write("++eoi 1\n")
+        #no CR, LF after write
+        ser.write("++eos 3\n")
+        #be controller, send to omicron
+        ser.write("++mode 1\n")
+        #no CR, LF with read
+        ser.write("++eos 3\n")
+
+    def read_print_config_gpib(self):
+        ''' get prologix gpib configuration'''
+        self.ser.write('++ver\n')
+        print( self.ser.readline())
+        self.ser.write("++auto\n")
+        print( 'auto '+self.ser.readline())
+        self.ser.write("++eoi\n")
+        print( 'eoi '+self.ser.readline())
+        self.ser.write("++eos\n")
+        print( 'eos '+self.ser.readline())
+        self.ser.write("++mode\n")
+        print( 'mode '+self.ser.readline())
+        self.ser.write("++addr\n")
+        print( 'address '+self.ser.readline())
+
+    def binary_escape_gpib_string( self, s ):
+        ''' prevent binary data from being interpreted
+        as prologix configuration commands, add lf'''
+        esc = chr(27)
+        lf = '\n'
+        cr = chr(0xd)
+        plus = '+'
+        
+        out = b''
+        for c in s:
+            if c in (esc, lf, cr, plus ):
+                out += esc
+            out += c
+        out += lf
+        return out
     
-    out = b''
-    for c in s:
-        if c in (esc, lf, cr, plus ):
-            out += esc
-        out += c
-    out += lf
-    return out
+    def write(self, s):
+        out = self.binary_escape_gpib_string(s)
+        if self.debug:
+            print("prologix write")
+            print("\t", " ".join(["{:02x}".format(ord(c)) for c in s]))
+            print("\t", " ".join(["{:08b}".format(ord(c)) for c in s]))            
+            
+        return self.ser.write(out)
 
-def write_gpib( port, s ):
-    out = gpib_string( s )
-    debug_omicron(out)
-    port.write( out )
-    
-def omicron_cmd_value( cmd, val ):
-    '''cmd is command number, val is uint 16, format for omicron '''
-    cmd &= 0x8f #commands are 0x8#
-    v1 = (val & 0xff00)>>8
-    v2 = val & 0xff
-    if v1 & 0x80:
-        cmd |= 0x40
-        v1  &= 0x7f
-    if v2 & 0x80:
-        cmd |= 0x20
-        v2  &= 0x7f
-    s = b'' + chr(cmd) + chr(v1) + chr(v2)
-    return s    
+from ScopeFoundry import BaseMicroscopeApp
+from ScopeFoundry.helper_funcs import load_qt_ui_file, sibling_path
 
-def set_omicron_volts( v ):
-    max = 2200.0
-    range = 0xffff
-    cmd = 0x82
-    if v > max: v = max
-    if v < 0: v = 0
-    val = int( v*range/max)
-    return omicron_cmd_value( cmd, val )
+class AugerElectronAnalyzerTestApp(BaseMicroscopeApp):
     
+    name = 'AugerElectronAnalyzerTestApp'
+    
+    def setup(self):
+        
+        AEA = self.add_hardware_component(AugerElectronAnalyzerHC(self))
+
+        self.ui.show()
+        
+        self.ui_analyzer = load_qt_ui_file(sibling_path(__file__, "auger_electron_analyzer_viewer.ui"))
+                
+        widget_connections = [
+         ('mode', 'retarding_mode_comboBox'),
+         ('multiplier', 'multiplier_checkBox'),
+         ('KE', 'KE_doubleSpinBox'),
+         ('work_function', 'work_func_doubleSpinBox'),
+         ('pass_energy', 'pass_energy_doubleSpinBox'),
+         ('crr_ratio', 'crr_ratio_doubleSpinBox'),
+         ('resolution', 'resolution_doubleSpinBox'),
+         ('quad_X1', 'qX1_doubleSpinBox'),
+         ('quad_Y1', 'qY1_doubleSpinBox'),
+         ('quad_X2', 'qX2_doubleSpinBox'),
+         ('quad_Y2', 'qY2_doubleSpinBox'),
+         ]
+        for lq_name, widget_name in widget_connections:
+            AEA.settings.get_lq(lq_name).connect_bidir_to_widget(getattr(self.ui_analyzer, widget_name))
+        
+        #self.ui_analyzer.show()
+        self.ui.centralwidget.layout().addWidget(self.ui_analyzer)
+        self.ui.setWindowTitle(self.name)
+        AEA.settings.connected.update_value(True)
+        
 if __name__ == '__main__':
-    #a = AugerElectronAnalyzer()
+    app = AugerElectronAnalyzerTestApp([])
+    app.exec_()
     
-    #a.write_state(True, 'CAE')
+    """a = AugerElectronAnalyzer(debug=True)
     
-    #a.ser.close()
+    a.write_state(True, 'CAE')
+    a.write_KE(2200)
+    time.sleep(10)
+    a.write_multiplier_state(False)
     
-    port = Serial("COM8")
-    set_config_gpib(port)
-    set_address_gpib(port,1)
-    get_config_gpib(port)
+    a.close()
+    """
+    
+    """
     write_gpib( port, set_mode_omicron(True, False) )
     write_gpib( port, set_omicron_volts( 1500 ))
     write_gpib( port, set_omicron_volts( 37.5 ))
@@ -317,4 +357,4 @@ if __name__ == '__main__':
     write_gpib( port, set_mode_omicron(mult=False) )
     
     port.close()
-    
+    """
