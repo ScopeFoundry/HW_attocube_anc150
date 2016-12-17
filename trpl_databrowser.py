d84b438c6ce3ad7fd3f7e324cb5ec652b1ea33a3
Edward Barnard <esbarnard@lbl.gov>
2016-12-17 15:33:18 -0800

    Picoharp_MCL_2DSlowScan_View: databrowser with ROI’s for slection of time histograms

--- trpl_databrowser.py
+++ trpl_databrowser.py
@@ -4,6 +4,7 @@ from viewers.apd_confocal_npz import ApdConfocalNPZView
 from viewers.picoharp_npz import PicoHarpNPZView
 from viewers.hyperspec_npz import HyperSpecNPZView
 from viewers.trpl_npz import TRPLNPZView
+from plimg_microscope.picoharp_mcl_2d_slow_scan import Picoharp_MCL_2DSlowScan_View
 
 
 if __name__ == '__main__':
@@ -14,5 +15,6 @@ if __name__ == '__main__':
     app.load_view(PicoHarpNPZView(app))
     app.load_view(HyperSpecNPZView(app))
     app.load_view(TRPLNPZView(app))
+    app.load_view(Picoharp_MCL_2DSlowScan_View(app))
     
     sys.exit(app.exec_())
