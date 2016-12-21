7fc28dad08f09f66942787bc5177ee8a3d354f17
Edward Barnard <esbarnard@lbl.gov>
2016-12-20 16:04:36 -0800

    DataBrowser: New viewers for DF microscope

--- df_microscope/winspec_remote_2Dscan.py
+++ df_microscope/winspec_remote_2Dscan.py
@@ -24,10 +24,14 @@ class WinSpecMCL2DSlowScan(MCLStage2DSlowScan):
         winspec_readout.run()
         
         if pixel_num == 0:
+            print("pixel 0: creating data arrays")
             spec_map_shape = self.scan_shape + winspec_readout.data.shape
             self.spec_map = np.zeros(spec_map_shape, dtype=np.float)
             self.spec_map_h5 = self.h5_meas_group.create_dataset('spec_map', spec_map_shape, dtype=np.float)
+            self.h5_file.flush()
             
+        if (pixel_num % 5) == 0:
+            self.h5_file.flush()
         
 
         #self.roi_data = self.picam.cam.reshape_frame_data(dat)
@@ -44,7 +48,35 @@ class WinSpecMCL2DSlowScan(MCLStage2DSlowScan):
         self.wavelength_h5 = self.h5_meas_group.create_dataset('wavelength', self.wavelength.shape, dtype=np.float)
         self.wavelength_h5[:] = self.wavelength[:]
         
+        self.h5_file.flush()
+        
     
     def update_display(self):
         MCLStage2DSlowScan.update_display(self)
         self.app.measurements.WinSpecRemoteReadout.update_display()
+
+from ScopeFoundry.data_browser import DataBrowser, HyperSpectralBaseView
+import h5py
+
+class WinSpecMCL2DSlowScanView(HyperSpectralBaseView):
+
+    name = 'WinSpecMCL2DSlowScan'
+    
+    def is_file_supported(self, fname):
+        return "WinSpecMCL2DSlowScan.h5" in fname   
+    
+    
+    def load_data(self, fname):    
+        self.dat = h5py.File(fname, 'r')
+        self.spec_map = np.squeeze(np.array(self.dat['/measurement/WinSpecMCL2DSlowScan/spec_map']), axis=(3,4))
+        
+        self.integrated_count_map =  self.spec_map.sum(axis=3)
+
+        self.hyperspec_data = self.spec_map[0] # pick frame 0
+        self.display_image = self.integrated_count_map[0]
+        self.spec_x_array = self.dat['/measurement/WinSpecMCL2DSlowScan/wavelength']
+        
+    def scan_specific_setup(self):
+        self.spec_plot.setLabel('left', 'Intensity', units='counts')
+        self.spec_plot.setLabel('bottom', 'Wavelength', units='nm')        
+        
--- trpl_databrowser.py
+++ trpl_databrowser.py
@@ -5,6 +5,9 @@ from viewers.picoharp_npz import PicoHarpNPZView
 from viewers.hyperspec_npz import HyperSpecNPZView
 from viewers.trpl_npz import TRPLNPZView
 from plimg_microscope.picoharp_mcl_2d_slow_scan import Picoharp_MCL_2DSlowScan_View
+from measurement_components.apd_confocal import APD_MCL_2DSlowScanZView
+from df_microscope.winspec_remote_2Dscan import WinSpecMCL2DSlowScanView
+from df_microscope.winspec_remote_readout import WinSpecRemoteReadoutView
 
 
 if __name__ == '__main__':
@@ -16,5 +19,8 @@ if __name__ == '__main__':
     app.load_view(HyperSpecNPZView(app))
     app.load_view(TRPLNPZView(app))
     app.load_view(Picoharp_MCL_2DSlowScan_View(app))
+    app.load_view(APD_MCL_2DSlowScanZView(app))
+    app.load_view(WinSpecMCL2DSlowScanView(app))
+    app.load_view(WinSpecRemoteReadoutView(app))
     
     sys.exit(app.exec_())
