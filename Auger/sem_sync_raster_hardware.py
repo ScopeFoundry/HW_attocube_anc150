83c467394bcb0c281123d9d833409617cbd21b5b
Edward Barnard <esbarnard@lbl.gov>
2017-02-17 09:34:05 -0800

    sem_sync_raster hardware and measurement updates

--- Auger/sem_sync_raster_hardware.py
+++ Auger/sem_sync_raster_hardware.py
@@ -8,11 +8,7 @@ Rewritten 2017-01-27 ESB
 '''
 from ScopeFoundry import HardwareComponent
 try:
-    from SEM.sem_equipment.raster_generator import RasterGenerator
-    from SEM.sem_equipment.rate_converter import RateConverter
-
     from ScopeFoundryHW.ni_daq import NI_SyncTaskSet
-    from ScopeFoundryHW.ni_daq.NI_CallBack import SyncCallBack
 except Exception as err:
     print("could not load modules needed for SemSyncRasterDAQ:", err)
 
@@ -26,26 +22,6 @@ class SemSyncRasterDAQ(HardwareComponent):
         self.display_update_period = 0.050 #seconds
 
         # Create logged quantities, set limits and defaults
-        #    several of these do not work as of 2/8/17
-        
-        self.sync_mode=self.add_logged_quantity('sync_mode',initial='regular',
-                                                dtype=str)
-            #FIX sync_mode = callback  broken
-        
-        self.callback_mode = self.add_logged_quantity("callback_mode", dtype=str, 
-                                                    ro=False,
-                                                    initial='line',
-                                                    choices=[('Slow','line'),('Fast','block')])
-            #FIX callback mode disabled
-        
-
-        self.scan_voltage = self.add_logged_quantity("scan_voltage", dtype=float, 
-                                                    ro=False, 
-                                                    initial=10.0, 
-                                                    vmin=0, 
-                                                    vmax=50.0,
-                                                    unit='V')
-            #FIX this does not appear to do anything
         
         self.sample_rate = self.add_logged_quantity("sample_rate", dtype=float, 
                                                     ro=False, 
@@ -62,27 +38,24 @@ class SemSyncRasterDAQ(HardwareComponent):
                                                     vmax=2e6,
                                                     unit='Hz')
         
-        self.sample_per_point = self.add_logged_quantity("sample_per_point", dtype=int, 
+        self.samples_per_pixel = self.add_logged_quantity("samples_per_pixel", dtype=int, 
                                                     ro=False, 
                                                     initial=1, 
                                                     vmin=1, 
                                                     vmax=1e10,
                                                     unit='samples')
         
-#         self.ms_per_unit=self.add_logged_quantity("ms_per_unit",dtype=float,
-#                                                   ro=False,
-#                                                   initial=3,
-#                                                   vmin=0.0005,
-#                                                   vmax=1e10)
-#         
-#         self.unit_of_rate=self.add_logged_quantity("unit_of_rate",dtype=int,
-#                                                    ro=False,
-#                                                    initial=1,
-#                                                    choices=[('ms/pixel',0),('ms/line',1),('ms/frame',2)])
-#         
+        self.continuous = self.add_logged_quantity('continuous', dtype=bool, initial=True)
+        
+                 
         self.output_channel_addresses= self.add_logged_quantity("output_channel_addresses",dtype=str,
                                                         ro=False,
                                                         initial='X-6363/ao0:1')
+        
+        self.trigger_output_term = self.add_logged_quantity('trigger_output_term', dtype=str,
+                                                            ro=False,
+                                                            initial="/X-6368/PXI_Trig0",
+                                                            )
         
         self.input_channel_addresses= self.add_logged_quantity("input_channel_addresses",dtype=str,
                                                         ro=False,
@@ -115,61 +88,46 @@ class SemSyncRasterDAQ(HardwareComponent):
 #                                                    dtype=bool,
 #                                                    ro=False)
 #         
-        self.timeout= self.add_logged_quantity("timeout",dtype=float,
-                                               ro=False,
-                                               initial=999,
-                                               vmin=1,
-                                               vmax=1e5,
-                                               unit='s')
         
         self.ext_clock_enable = self.add_logged_quantity("ext_clock_enable", dtype=bool, initial=False)
         self.ext_clock_source = self.add_logged_quantity("ext_clock_source", dtype=str, initial="/X-6368/PFI0")
         
-             #parameters that cannot change during image acquisition
+        #parameters that cannot change during while connected
         self.lq_lock_on_connect = ['output_channel_addresses', 'input_channel_addresses',
                                     'counter_channel_addresses', 'counter_channel_terminals',
-                                    'ext_clock_source']
+                                    'ext_clock_source', 'trigger_output_term']
        
         
     def connect(self):        
         if self.debug_mode.val: self.log.debug( "connecting to {}".format(self.name))
         
-
         #self.remcon=self.app.hardware['sem_remcon']        
-        
-#         self.scan_voltage.update_value(10.0)
-        
+                
         # lock logged quantities during connection
         for lqname in self.lq_lock_on_connect:
             lq = self.settings.get_lq(lqname)
             lq.change_readonly(True)
 
         #setup tasks
-        if self.sync_mode.val=='regular':
-            if self.settings['ext_clock_enable']:
-                clock_source = self.settings['ext_clock_source']
-            else:
-                clock_source = "" 
-            self.sync_analog_io = NI_SyncTaskSet(out_chan  = self.output_channel_addresses.val,
-                                       in_chan   = self.input_channel_addresses.val,
-                                       ctr_chans = self.counter_channel_addresses.val.split(','),
-                                       ctr_terms = self.counter_channel_terminals.val.split(','),
-                                       clock_source = clock_source,
-                                       trigger_output_term = "/X-6368/PXI_Trig0",
-                                       )
-            # FIX this is broken
-        elif self.sync_mode.val=='callback':
-            self.sync_analog_io= SyncCallBack(self.output_channel_addresses.val,self.input_channel_addresses.val,self.counter_channel_addresses.val.split(','),self.counter_channel_terminals.val.split(','))
-      
+        if self.settings['ext_clock_enable']:
+            clock_source = self.settings['ext_clock_source']
+        else:
+            clock_source = "" 
+        self.sync_analog_io = NI_SyncTaskSet(out_chan  = self.output_channel_addresses.val,
+                                   in_chan   = self.input_channel_addresses.val,
+                                   ctr_chans = self.counter_channel_addresses.val.split(','),
+                                   ctr_terms = self.counter_channel_terminals.val.split(','),
+                                   clock_source = clock_source,
+                                   trigger_output_term = self.trigger_output_term.val,
+                                   )
         self.ctr_num=2
         
         #from sample per point and sample rate, calculate the output(scan rate)
-        #self.output_rate.update_value(self.sample_rate.val/self.sample_per_point.val)
+        #self.output_rate.update_value(self.sample_rate.val/self.samples_per_pixel.val)
         
         self.adc_chan_count  = self.sync_analog_io.get_adc_chan_count()
         
-        #self.setup_io()
-
+        
     def disconnect(self):
         
         for lqname in self.lq_lock_on_connect:
@@ -177,9 +135,7 @@ class SemSyncRasterDAQ(HardwareComponent):
             lq.change_readonly(False)
 
         #disconnect logged quantities from hardware
-        for lq in self.settings.as_dict().values():
-            lq.hardware_read_func = None
-            lq.hardware_set_func = None
+        self.settings.disconnect_all_from_hardware()
 
         #disconnect hardware
         if hasattr(self, "sync_analog_io"):
@@ -191,28 +147,29 @@ class SemSyncRasterDAQ(HardwareComponent):
 
        
     def setup_io_with_data(self, X, Y):
+        """
+        Set up sync task with X and Y arrays sent to the analog output channels
+        Compute output rate based on settings 
+        """
         assert len(X) == len(Y)        
         self.num_pixels = len(X)
-        self.num_samples = int(self.num_pixels *self.sample_per_point.val)
-        self.pixel_time = self.sample_per_point.val / self.sample_rate.val        
-        self.timeout.update_value(1.5 * self.pixel_time * self.num_pixels  )            
-        self.output_rate.update_value(self.sample_rate.val/self.sample_per_point.val)
-         
-        if self.sync_mode.val=='regular':
-            finite = True
-        else: # callback (hao version)
-            finite = False
+        self.num_samples = int(self.num_pixels *self.samples_per_pixel.val)
+        self.pixel_time = self.samples_per_pixel.val / self.sample_rate.val        
+        self.timeout = 1.5 * self.pixel_time * self.num_pixels             
+        self.output_rate.update_value(self.sample_rate.val/self.samples_per_pixel.val)
+        
         
         self.sync_analog_io.setup(rate_out = self.output_rate.val,
                                       count_out = self.num_pixels, 
                                       rate_in = self.sample_rate.val,
                                       count_in = self.num_samples, 
                                       #pad = ?,
-                                      is_finite=finite)
+                                      is_finite=(not self.continuous.val))
         
         self.XY = self.interleave_xy_arrays(X, Y)                
         self.sync_analog_io.write_output_data_to_buffer(self.XY)
         
+    
     def interleave_xy_arrays(self, X, Y):
         """take 1D X and Y arrays to create a flat interleaved XY array
         of the form [x0, y0, x1, y1, .... xN, yN]
@@ -224,69 +181,81 @@ class SemSyncRasterDAQ(HardwareComponent):
         XY[1::2] = Y
         return XY       
           
-   
+
+    def read_ai_chan_pixels(self, n_pixels):
+        # Grabs n_pixels worth of multi-channel, multi-sample 
+        # data shaped as (n_pixels, n_chan, n_samp)
+        # TODO: check if n_pixels worth of data are actually returned
+        n_samples = int(n_pixels * self.samples_per_pixel.val)
+        buf = self.sync_analog_io.read_adc_buffer(count = n_samples, timeout=self.timeout)
+        #print('read_ai_chan_pixels', 'n_pixels', n_pixels, 'adc_chan_count', self.adc_chan_count, 
+        #      'n_samples', n_samples, 'buf.shape', buf.shape)
+
+        return buf.reshape(n_pixels, self.samples_per_pixel.val, self.adc_chan_count).swapaxes(1,2)
+        
+    
+    def read_counter_buffer(self, i):
+        return self.sync_analog_io.read_ctr_buffer_diff(i, timeout=self.timeout)
+
+    def start(self):
+        # TODO disable LQ's that can't be changed during task run
+        self.sync_analog_io.start()
+        
+    def stop(self):
+        # TODO re-enable LQ's that can't be changed during task run
+        self.sync_analog_io.stop()
+        
+        
+    def set_adc_n_pixel_callback(self, n_pixels, cb_func):
+        """
+        Setup callback functions for EveryNSamplesEvent
+        *cb_func* will be called 
+        after every *n_pixels* are acquired. 
+        """
+        n_samples = n_pixels*self.samples_per_pixel.val
+        self.sync_analog_io.adc.set_n_sample_callback(n_samples, cb_func)
+        
+
+    #def read_counters(self):
+    
+    # replaced by callback-based measurement 2/9/17
+    # keep as as reference for single frame measurement
+    """
     def single_scan_data_block(self):
         self.ai_data = self.read_ai_chans()
             #handle oversampled ADC data
         self.ai_data =\
-            self.ai_data.reshape(-1,self.sample_per_point.val,self.adc_chan_count)
+            self.ai_data.reshape(-1,self.samples_per_pixel.val,self.adc_chan_count)
         self.ai_data = self.ai_data.mean(axis=1)
         
         self.sync_analog_io.stop()
         #self.scanDAQ.sync_analog_io.close()
         return self.ai_data
 
-        
-    def read_ai_buffer(self):
-        # interleaved buffer
-        return self.sync_analog_io.read_adc_buffer(timeout=self.timeout.val)
-    
-    def read_ai_chans(self):
-        return self.sync_analog_io.read_adc_buffer_reshaped(timeout=self.timeout.val)
-    
-    def read_ai_chan_pixels(self, n_pixels):
-        # Grabs n_pixels worth of multi-channel, multi-sample 
-        # data shaped as (n_pixels, n_chan, n_samp)
-        n_samples = int(n_pixels * self.sample_per_point.val)
-        buf = self.sync_analog_io.read_adc_buffer(count = n_samples, timeout=self.timeout.val)
-        print('read_ai_chan_pixels', n_pixels, self.adc_chan_count, n_samples, buf.shape)
-
-        return buf.reshape(n_pixels, self.sample_per_point.val, self.adc_chan_count).swapaxes(1,2)
-        
-    
-    def read_counter_buffer(self, i):
-        return self.sync_analog_io.read_ctr_buffer_diff(i, timeout=self.timeout.val)
-
-    def start(self):
-        self.sync_analog_io.start()
-        
-    def stop(self):
-        self.sync_analog_io.stop()
-
-    #def read_counters(self):
-    
-    
-
-
-    #replaced by block graphics update 2/9/17
+    # replaced by callback-based measurement 2/9/17
+    # keep as as reference for single frame measurement
     def single_scan_regular(self, X_pos, Y_pos):
         #connect to SEM scanner module, which calculates the voltage output,
         #create detector channels and creates the scanning task
-            #overrides lq to force "regular" mode
 
         self.setup_io_with_data(X_pos, Y_pos)
         self.sync_analog_io.start()            
         
-        #self.images.read_all() -- old way everything hiding in image_display object
-
         self.ai_data = self.read_ai_chans()
             #handle oversampled ADC data
         self.ai_data =\
-            self.ai_data.reshape(-1,self.sample_per_point.val,self.sync_analog_io.get_adc_chan_count())
+            self.ai_data.reshape(-1,self.samples_per_pixel.val,self.sync_analog_io.get_adc_chan_count())
         self.ai_data = self.ai_data.mean(axis=1)
         
         self.sync_analog_io.stop()
         #self.scanDAQ.sync_analog_io.close()
         return self.ai_data
+    
+    def read_ai_buffer(self):
+        # interleaved buffer
+        return self.sync_analog_io.read_adc_buffer(timeout=self.timeout)
+    
+    def read_ai_chans(self):
+        return self.sync_analog_io.read_adc_buffer_reshaped(timeout=self.timeout)
 
-        
+    """
--- Auger/sem_sync_raster_measure.py
+++ Auger/sem_sync_raster_measure.py
@@ -7,7 +7,6 @@ ESB 2016-07-19
 '''
 
 from ScopeFoundry.scanning.base_cartesian_scan import BaseCartesian2DScan
-from SEM.sem_equipment.rate_converter import RateConverter
 import numpy as np
 import time
 
@@ -24,7 +23,8 @@ class SemSyncRasterScan(BaseCartesian2DScan):
         self.display_update_period = 0.050 #seconds
 
         # Created logged quantities
-            #FIX what does this do dfo 2/10/17
+        
+        #FIX what does this do dfo 2/10/17
         self.recovery_time = self.add_logged_quantity("recovery_time", dtype=float, 
                                                     ro=True,
                                                     initial=0.07)
@@ -33,12 +33,11 @@ class SemSyncRasterScan(BaseCartesian2DScan):
         self.scanDAQ = self.app.hardware['SemSyncRasterDAQ']        
         self.scan_on=False
         
-        if hasattr(self.gui,'sem_remcon'):#FIX re-implement later
+        if hasattr(self.app,'sem_remcon'):#FIX re-implement later
             self.sem_remcon=self.app.sem_remcon
         
 
     def run(self):
-        self.current_scan_index = 0,0,0
         # Compute data arrays
         self.log.debug( "computing scan arrays")
         self.compute_scan_arrays()
@@ -74,56 +73,106 @@ class SemSyncRasterScan(BaseCartesian2DScan):
         
  
         try:
-            while not self.interrupt_measurement_called:
-                self.pixel_index = 0
-                
-                #### old get full image while blocking measurement thread
-                #self.ai_data = self.scanDAQ.single_scan_regular(self.scan_h_positions, -1*self.scan_v_positions)
-                #self.display_image_map[0,:,:] = self.ai_data[:,1].reshape(self.settings['Nv'], self.settings['Nh'])       
-                ####
-                
-                self.scanDAQ.setup_io_with_data(self.scan_h_positions, -1*self.scan_v_positions)
-                #compute pixel display increment
-                # need at least one
-                num_pixels_per_block = max(1, int(np.round(0.010 / self.scanDAQ.pixel_time)))
-                
-                # Data array
-                self.adc_pixels = np.zeros((self.Npixels, self.scanDAQ.adc_chan_count), dtype=float)
-                
-                self.scanDAQ.start()
-                
-                while self.pixel_index < self.Npixels:
-                    print('block', self.pixel_index, num_pixels_per_block,  self.Npixels)
-                    if self.interrupt_measurement_called:
-                        break
-                    remaining_pixels = self.Npixels - self.pixel_index
-                    dii = acq_n_pixel = min(remaining_pixels, num_pixels_per_block)
-                    
-                    print('block', self.pixel_index, num_pixels_per_block,  self.Npixels, remaining_pixels)
+            self.pixel_index = 0
+            self.current_scan_index = self.scan_index_array[0]
+            
+            #### old get full image while blocking measurement thread
+            #self.ai_data = self.scanDAQ.single_scan_regular(self.scan_h_positions, -1*self.scan_v_positions)
+            #self.display_image_map[0,:,:] = self.ai_data[:,1].reshape(self.settings['Nv'], self.settings['Nh'])       
+            ####
+            
+            self.scanDAQ.setup_io_with_data(self.scan_h_positions, -1*self.scan_v_positions)
+            
+            #compute pixel acquisition block size 
+            # need at least one, and needs to an integer divisor of Npixels
+            
+            num_pixels_per_block = max(1, int(np.ceil(self.display_update_period / self.scanDAQ.pixel_time)))
+            if num_pixels_per_block > self.Nh.val:
+                num_pixels_per_block = self.Nh.val*np.ceil( num_pixels_per_block / self.Nh.val )
+            #int(ceilself.Npixels/int(np.floor(self.Npixels/num_pixels_per_block)))
 
-                    
-                    # read num_pixels_per_block
-                    buf = self.scanDAQ.read_ai_chan_pixels(acq_n_pixel) # shape (n_pixels, n_chan, n_samp)
-                    print(buf.shape)
-                    # average over samples
-                    buf = buf.mean(axis=2)
-                    
-                    
-                    
-                    #stuff into pixel data array
-                    ii = self.pixel_index
-                    self.adc_pixels[ii: ii + dii] = buf
-                    
-                    DISPLAY_CHAN = 1
-                    self.display_image_map[ self.scan_index_array[ii:ii+dii] ] = self.adc_pixels[ii:ii+dii, DISPLAY_CHAN]
-                    
-                    self.pixel_index += acq_n_pixel 
+            num_blocks = int(max(1, np.floor(self.Npixels / num_pixels_per_block)))
+            
+            while self.Npixels % num_blocks != 0:
+                num_blocks -= 1
+                print("num_blocks", num_blocks)
+            
+            
+            self.num_pixels_per_block = num_pixels_per_block = int(self.Npixels / num_blocks)
+            print("num_pixels_per_block", num_pixels_per_block)
+            #asdf
+            
+            # Data array
+            self.adc_pixels = np.zeros((self.Npixels+2, self.scanDAQ.adc_chan_count), dtype=float)
+            self.pixels_remaining = self.Npixels
+            self.task_done = False
+            
+            def every_n_callback_func():
+                new_data = self.scanDAQ.read_ai_chan_pixels(num_pixels_per_block)
+                on_new_data(new_data)
+                return 0
+            
+            #def frame_done_callback_func():
+            #    new_data = self.scanDAQ.read_ai_chan_pixels(self.pixels_remaining)
+            #    on_new_data(new_data)
+            #    return 0
 
-                # TODO read Counters
-                # FIX handle serpentine scans
-                #self.display_image_map[self.scan_index_array] = self.ai_data[0,:]
-                # TODO save data
-                self.scanDAQ.stop()
+            def done_callback_func(status):
+                self.task_done = True
+                print("done", status)
+                return 0
+            
+            def on_new_data(new_data):
+                self.set_progress(100*self.pixel_index / self.Npixels )
+                #print('callback block', self.pixel_index, new_data.shape, 'remaining px', self.Npixels - self.pixel_index)
+                new_data = new_data.reshape(-1,  self.scanDAQ.samples_per_pixel.val, self.scanDAQ.adc_chan_count).swapaxes(1,2)
+                ii = self.pixel_index
+                dii = num_new_pixels = new_data.shape[0]
+                # average over samples
+                new_data = new_data.mean(axis=2)
+                #print('new_data', new_data.shape)
+
+                #stuff into pixel data array
+                #print('ii', ii, 'dii', dii,  (self.adc_pixels[ii: ii + dii]).shape)
+                self.adc_pixels[ii: ii + dii] = new_data
+                
+                DISPLAY_CHAN = 1                    
+                x = self.scan_index_array[ii:ii+dii,:].T
+                x1 = self.scan_index_array[(ii+dii+1)%self.Npixels,:]
+                self.display_image_map[x[0], x[1], x[2]] = self.adc_pixels[ii:ii+dii, DISPLAY_CHAN]
+                #self.display_image_map[x1[0], x1[1], x1[2]] = 1000.0
+                #print("self.display_image_map[ self.scan_index_array[ii:ii+dii] ]", self.display_image_map[ self.scan_index_array[ii:ii+dii] ].shape)
+                #print("self.adc_pixels[ii:ii+dii, DISPLAY_CHAN]", self.adc_pixels[ii:ii+dii, DISPLAY_CHAN])
+                
+                self.current_scan_index = self.scan_index_array[self.pixel_index]
+
+                self.pixel_index += num_new_pixels
+                
+                self.pixel_index %= self.Npixels
+                
+                self.pixels_remaining = self.Npixels - self.pixel_index
+                
+                
+
+            # register callbacks
+            self.scanDAQ.set_adc_n_pixel_callback(num_pixels_per_block, every_n_callback_func)
+            self.scanDAQ.sync_analog_io.adc.set_done_callback(done_callback_func )
+            
+            self.scanDAQ.start()
+            
+            while not self.task_done and not self.interrupt_measurement_called:
+                time.sleep(0.1)
+                            
+            # TODO read Counters
+            # FIX handle serpentine scans
+            #self.display_image_map[self.scan_index_array] = self.ai_data[0,:]
+            # TODO save data
+            self.scanDAQ.stop()
+            print("Npixels", self.Npixels, 'block size', self.num_pixels_per_block, 'num_blocks', num_blocks)
+            print("pixels remaining:", self.pixels_remaining)
+            print("blocks_per_sec",1.0/ (self.scanDAQ.pixel_time*num_pixels_per_block))
+            print("frames_per_sec",1.0/ (self.scanDAQ.pixel_time*self.Npixels))
+
         finally:
             pass
         
@@ -170,6 +219,8 @@ class SemSyncRasterScan(BaseCartesian2DScan):
         self.scan_on=False       
     
     def update_display(self):
+        kk,jj, ii = self.scan_index_array[self.pixel_index]
+        self.current_stage_pos_arrow.setPos(self.h_array[ii], self.v_array[jj])
         BaseCartesian2DScan.update_display(self)
         
     
