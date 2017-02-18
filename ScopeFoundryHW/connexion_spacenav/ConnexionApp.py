8ba2ccf5ab5ebbbdc03dfb81d04502a8c3a93670
Alan Buckley <alanbuckley@lbl.gov>
2017-02-17 17:36:41 -0800

    Wrote entirely new wrapper. Module now fixed restructured, and thorougly documented. Licensing and readme added.

--- ScopeFoundryHW/connexion_spacenav/ConnexionApp.py
+++ ScopeFoundryHW/connexion_spacenav/ConnexionApp.py
@@ -1,16 +1,23 @@
-#from PySide import QtGui
+'''
+Connexion SpaceNavigator/SpaceMouse ScopeFoundry module
+@author: Alan Buckley
+
+Suggestions for improvement from Ed Barnard. <esbarnard@lbl.gov>
+
+'''
 import sys
 from ScopeFoundry import BaseMicroscopeApp
-#import spacenavigator
-from hardware.connexion_hc import Connexion_HC
-from measurement.connexion_mc import Connexion_MC
+
 
 class ConnexionApp(BaseMicroscopeApp):
 	
 	def setup(self):
-		self.connexion_hc = self.add_hardware_component(Connexion_HC(self))
 
-		self.connexion_mc = self.add_measurement_component(Connexion_MC(self))
+		from ScopeFoundryHW.connexion_spacenav.connexion_hc import Connexion_HC
+		self.connexion_hc = self.add_hardware(Connexion_HC(self))
+
+		from ScopeFoundryHW.connexion_spacenav.connexion_mc import Connexion_MC
+		self.connexion_mc = self.add_measurement(Connexion_MC(self))
 		self.ui.show()
 		self.ui.activateWindow()
 
