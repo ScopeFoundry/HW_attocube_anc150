0e53ec24658340b999bb7de76d46cf49dfdc7976
Edward Barnard <esbarnard@lbl.gov>
2017-02-24 10:51:42 -0800

    fixed counter acquisition for quad SEM mode

--- Auger/sem_sync_raster_measure.py
+++ Auger/sem_sync_raster_measure.py
@@ -39,7 +39,16 @@ class SemSyncRasterScan(BaseRaster2DScan):
             self.sem_remcon=self.app.sem_remcon
         
     def run(self):
-        # Compute data arrays
+        # if hardware is not connected, connect it
+        # we need to wait while the task is created before 
+        # measurement thread continues
+        if not self.scanDAQ.settings['connected']:
+            #self.scanDAQ.connect()
+            self.scanDAQ.settings['connected'] = True
+            #self.app.qtapp.processEvents()         
+            time.sleep(0.2)
+
+        # Compute data arrays        
         self.log.debug( "computing scan arrays")
         self.compute_scan_arrays()
         self.log.debug( "computing scan arrays... done")
@@ -65,16 +74,12 @@ class SemSyncRasterScan(BaseRaster2DScan):
                         
         # previously set samples_per_point in scanDAQ hardware
                
-        # if hardware is not connected, connect it
-        # we need to wait while the task is created before 
-        # measurement thread continues
-        if not self.scanDAQ.settings.connected:
-            self.scanDAQ.settings.connected.update_value(True)         
-            time.sleep(0.2)
+
         
  
         try:
-            self.pixel_index = 0
+            self.pixel_index = 0# contains index of next adc pixel to be moved from queue into adc_pixels
+
             self.current_scan_index = self.scan_index_array[0]
             
             #### old get full image while blocking measurement thread
@@ -104,28 +109,39 @@ class SemSyncRasterScan(BaseRaster2DScan):
             ##### Data array
             self.adc_pixels = np.zeros((self.Npixels, self.scanDAQ.adc_chan_count), dtype=float)
             self.pixels_remaining = self.Npixels # in frame
-            self.new_data_queue = []
+            self.new_adc_data_queue = [] # will contain numpy arrays (data blocks) from adc to be processed
+            self.adc_map = np.zeros(self.scan_shape + (self.scanDAQ.adc_chan_count,), dtype=float)
+            
+            # contains index of next pixel to be processed, need one per ctr
+            # since ctrs are independent tasks
+            self.ctr_pixel_index = np.zeros(self.scanDAQ.ctr_num, dtype=int) 
+            self.ctr_pixels = np.zeros((self.Npixels, self.scanDAQ.ctr_num), dtype=int)
+            self.new_ctr_data_queue = [] # list will contain tuples (ctr_number, data_block) to be processed
+            self.ctr_map = np.zeros(self.scan_shape + (self.scanDAQ.ctr_num,), dtype=int)
+            self.ctr_map_Hz = np.zeros(self.ctr_map.shape, dtype=float)
             
             self.task_done = False
             
             # register callbacks
             self.scanDAQ.set_adc_n_pixel_callback(
-                num_pixels_per_block, self.every_n_callback_func)
+                num_pixels_per_block, self.every_n_callback_func_adc)
             self.scanDAQ.sync_analog_io.adc.set_done_callback(
-                self.done_callback_func )
-                        
+                self.done_callback_func_adc )
+            
+            for ctr_i in range(self.scanDAQ.ctr_num):
+                self.scanDAQ.set_ctr_n_pixel_callback( ctr_i,
+                        num_pixels_per_block, lambda i=ctr_i: self.every_n_callback_func_ctr(i))
             
             self.pre_scan_setup()
 
             #### Start scan daq 
             self.scanDAQ.start()
             
-            #### Wait until done
+            #### Wait until done, while processing data queues
             while not self.task_done and not self.interrupt_measurement_called:
                 self.handle_new_data()
                 time.sleep(self.display_update_period)
                             
-            # TODO read Counters
             # FIX handle serpentine scans
             #self.display_image_map[self.scan_index_array] = self.ai_data[0,:]
             # TODO save data
@@ -141,65 +157,74 @@ class SemSyncRasterScan(BaseRaster2DScan):
 
             self.post_scan_cleanup()
 
-            pass
         
     
     def update_display(self):
+        self.get_display_pixels()
         x = self.scan_index_array.T
         self.display_image_map[x[0], x[1], x[2]] = self.display_pixels
 
         kk,jj, ii = self.scan_index_array[self.pixel_index]
-        self.current_stage_pos_arrow.setPos(self.h_array[ii], self.v_array[jj])
+        #self.current_stage_pos_arrow.setPos(self.h_array[ii], self.v_array[jj])
+        self.current_stage_pos_arrow.setVisible(False)
+        t0 = time.time()
         BaseRaster2DScan.update_display(self)
-    
+        #print("sem_sync_raster_scan timing {}".format(time.time()-t0))
     
     ##### Callback functions
-    def every_n_callback_func(self):
-        new_data = self.scanDAQ.read_ai_chan_pixels(
+    def every_n_callback_func_adc(self):
+        new_adc_data = self.scanDAQ.read_ai_chan_pixels(
             self.num_pixels_per_block)
-        self.new_data_queue.append(new_data)
-        #self.on_new_data(new_data)
+        self.new_adc_data_queue.append(new_adc_data)
+        #self.on_new_adc_data(new_data)
         return 0
     
-    def done_callback_func(self, status):
+    def every_n_callback_func_ctr(self, ctr_i):
+        new_ctr_data = self.scanDAQ.read_counter_buffer(
+            ctr_i, self.num_pixels_per_block)
+        self.new_ctr_data_queue.append( (ctr_i, new_ctr_data))
+        #print("every_n_callback_func_ctr {} {}".format(ctr_i, len(new_ctr_data)))
+        return 0
+            
+    def done_callback_func_adc(self, status):
         self.task_done = True
         print("done", status)
         return 0
     
     def handle_new_data(self):
-        while len(self.new_data_queue) > 0:
+        while len(self.new_adc_data_queue) > 0:
             # grab the next available data chunk
-            #print('new_data_queue' + "[===] "*len(self.new_data_queue))
-            new_data = self.new_data_queue.pop(0)
-            self.on_new_data(new_data)
+            #print('new_adc_data_queue' + "[===] "*len(self.new_adc_data_queue))
+            new_data = self.new_adc_data_queue.pop(0)
+            self.on_new_adc_data(new_data)
+            if self.interrupt_measurement_called:
+                break
+
+        while len(self.new_ctr_data_queue) > 0:
+            ctr_i, new_data = self.new_ctr_data_queue.pop(0)
+            self.on_new_ctr_data(ctr_i, new_data)
             if self.interrupt_measurement_called:
                 break
 
     
-    def on_new_data(self, new_data):
+    def on_new_adc_data(self, new_data):
         self.set_progress(100*self.pixel_index / self.Npixels )
         #print('callback block', self.pixel_index, new_data.shape, 'remaining px', self.Npixels - self.pixel_index)
         new_data = new_data.reshape(-1,  self.scanDAQ.samples_per_pixel.val, self.scanDAQ.adc_chan_count).swapaxes(1,2)
         ii = self.pixel_index
         dii = num_new_pixels = new_data.shape[0]
-        # average over samples
+        # average over samples (takes oversampled adc data and
+        # gives single pixel average for each channel)
         new_data = new_data.mean(axis=2)
-        #print('new_data', new_data.shape)
 
         #stuff into pixel data array
-        #print('ii', ii, 'dii', dii,  (self.adc_pixels[ii: ii + dii]).shape)
-        self.adc_pixels[ii: ii + dii] = new_data
+        self.adc_pixels[ii:ii+dii , :] = new_data
         
-        DISPLAY_CHAN =1
-        self.display_pixels = self.adc_pixels[:,DISPLAY_CHAN]
         """        DISPLAY_CHAN = 1                    
         x = self.scan_index_array[ii:ii+dii,:].T
         x1 = self.scan_index_array[(ii+dii+1)%self.Npixels,:]
         self.display_image_map[x[0], x[1], x[2]] = self.adc_pixels[ii:ii+dii, DISPLAY_CHAN]
         """
-        #self.display_image_map[x1[0], x1[1], x1[2]] = 1000.0
-        #print("self.display_image_map[ self.scan_index_array[ii:ii+dii] ]", self.display_image_map[ self.scan_index_array[ii:ii+dii] ].shape)
-        #print("self.adc_pixels[ii:ii+dii, DISPLAY_CHAN]", self.adc_pixels[ii:ii+dii, DISPLAY_CHAN])
         
         self.current_scan_index = self.scan_index_array[self.pixel_index]
 
@@ -208,9 +233,35 @@ class SemSyncRasterScan(BaseRaster2DScan):
         self.pixel_index %= self.Npixels
         
         self.pixels_remaining = self.Npixels - self.pixel_index
+        
+        # copy data to image shaped map
+        x = self.scan_index_array[ii:ii+dii,:].T
+        self.adc_map[x[0], x[1], x[2],:] = new_data
+
+
+    def on_new_ctr_data(self, ctr_i, new_data):
+        #print("on_new_ctr_data {} {}".format(ctr_i, new_data))
+        ii = self.ctr_pixel_index[ctr_i]
+        dii = num_new_pixels = new_data.shape[0]
+        
+        self.ctr_pixels[ii: ii+dii, ctr_i] = new_data
+        
+        self.ctr_pixel_index[ctr_i] += dii
+        self.ctr_pixel_index[ctr_i] %= self.Npixels
+
+        # copy data to image shaped map
+        x = self.scan_index_array[ii:ii+dii,:].T
+        self.ctr_map[x[0], x[1], x[2], ctr_i] = new_data
+        self.ctr_map_Hz[x[0], x[1], x[2], ctr_i] = new_data *1.0/ self.scanDAQ.pixel_time
+        
 
     def pre_scan_setup(self):
         pass
 
     def post_scan_cleanup(self):
         pass
+    
+    def get_display_pixels(self):
+        DISPLAY_CHAN = 0
+        self.display_pixels = self.adc_pixels[:,DISPLAY_CHAN]
+        #self.display_pixels = self.ctr_pixels[:,DISPLAY_CHAN]
